15d38d5a625cc02056e8887be52e1c4b77c95ca1
Alessandro <alessandro.verna@studio.unibo.it>
2023-03-16 23:10:05 +0100

    Method getHeight() introduced

--- src/main/java/it/unibo/jumpig/model/api/Camera.java
+++ src/main/java/it/unibo/jumpig/model/api/Camera.java
@@ -4,4 +4,9 @@ package it.unibo.jumpig.model.api;
  * Interface Camera.
  */
 public interface Camera {
+    /**
+     * Getter that returns the camera's height.
+     * @return an Integer that represents the camera's height
+     */
+    Integer getHeight();
 }
