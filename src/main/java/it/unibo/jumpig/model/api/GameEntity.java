08c4c307cc5f2bb034ff98b42442a5a13c37db5d
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-03-16 20:21:45 +0100

    Corrected Javadoc in GameEntity

--- src/main/java/it/unibo/jumpig/model/api/GameEntity.java
+++ src/main/java/it/unibo/jumpig/model/api/GameEntity.java
@@ -6,7 +6,6 @@ import it.unibo.jumpig.common.api.Position;
  * Interface that represent an entity in the game.
  */
 public interface GameEntity {
-    
     /**
      * Getter for the position of the gameEntity.
      * @return the gameEntity's position.
