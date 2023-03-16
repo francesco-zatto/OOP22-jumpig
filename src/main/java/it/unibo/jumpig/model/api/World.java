cb8f90a8a7844ead743e6f2c63da5051ec1b1267
Alessandro <alessandro.verna@studio.unibo.it>
2023-03-16 23:00:05 +0100

    Method getGravity() added

--- src/main/java/it/unibo/jumpig/model/api/World.java
+++ src/main/java/it/unibo/jumpig/model/api/World.java
@@ -4,4 +4,9 @@ package it.unibo.jumpig.model.api;
  * interface World.
  */
 public interface World {
+    /**
+     * getter for game gravity.
+     * @return the value of the gravity
+     */
+    Integer getGravity();
 }
