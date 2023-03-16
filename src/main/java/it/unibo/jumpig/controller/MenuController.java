7727ebd586e3eae9a338a466df31e8bb468de218
2L_gigi <luigi.li@studio.unibo.it>
2023-03-16 21:10:45 +0100

    Added javadoc comments in controller

---
+++ src/main/java/it/unibo/jumpig/controller/MenuController.java
@@ -0,0 +1,18 @@
+package it.unibo.jumpig.controller.api;
+
+/**
+ * Game controller
+ */
+
+public interface MenuController {
+    /**
+     * The method notify when the game starts
+     */
+    void notifyStartGame();
+
+    /**
+     * The method notify when the leaderboard starts
+     */
+    void notifyStartLeaderboard();
+
+}
