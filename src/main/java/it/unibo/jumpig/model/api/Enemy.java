1fa7a162d7b9f1553ae1f2023088c0c532d4ee60
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-03-16 22:10:28 +0100

    Created the interface Enemy

---
+++ src/main/java/it/unibo/jumpig/model/api/Enemy.java
@@ -0,0 +1,11 @@
+package it.unibo.jumpig.model.api;
+
+/**
+ * An interface that represents the enemy of the game, an entity which
+ * the player should avoid, otherwise he will lose a life, or worse, die. 
+ */
+public interface Enemy extends GameEntity {
+    
+
+
+}
