0fab806815881f0a67a31bc13ad758fc9604bada
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-03-16 17:12:55 +0100

    Added javadoc comments in Position

--- src/main/java/it/unibo/jumpig/common/api/Position.java
+++ src/main/java/it/unibo/jumpig/common/api/Position.java
@@ -1,11 +1,18 @@
 package it.unibo.jumpig.common.api;
 
 /**
- * An interface that represents the position in a Cartesian Plane..
+ * An interface that represents the position in a Cartesian Plane.
  */
 public interface Position {
-    
+    /**
+     * Getter for the x, the coordinate in the horizontal axis.
+     * @return the coordinate in the x axis.
+     */
     double getX();
 
+    /**
+     * Getter for the y, the coordinate in the horizontal axis.
+     * @return the coordinate in the y axis.
+     */
     double getY();
 }
