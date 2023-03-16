bb210b311c36a2fc38080a3df0eebb395cf837d1
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-03-16 16:29:22 +0100

    Created Position with its getters

---
+++ src/main/java/it/unibo/jumpig/common/api/Position.java
@@ -0,0 +1,11 @@
+package it.unibo.jumpig.common.api;
+
+/**
+ * An interface that represents the position in a Cartesian Plane..
+ */
+public interface Position {
+    
+    double getX();
+
+    double getY();
+}
