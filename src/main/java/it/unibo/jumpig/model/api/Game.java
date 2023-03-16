5c70629d34a9fa7bf53e2446c98cad4071319a84
Alessandro <alessandro.verna@studio.unibo.it>
2023-03-16 18:04:59 +0100

    added javadoc for methods

--- src/main/java/it/unibo/jumpig/model/api/Game.java
+++ src/main/java/it/unibo/jumpig/model/api/Game.java
@@ -1,14 +1,23 @@
 package it.unibo.jumpig.model.api;
 
+/**
+ * interface Game.
+ */
 public interface Game {
-    
     /**
      * this method checks if the game is over.
      * @return the game status
      */
     boolean isOVer();
 
+    /**
+     * this method updates positions for every entity in a game.
+     */
     void updateGame();
 
+    /**
+     * this method returns the player's final score.
+     * @return the player's score
+     */
     int getCurrentScore();
 }
