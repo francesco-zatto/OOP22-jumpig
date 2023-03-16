e18e6bf32291916073753f8670b3f74d30526f4b
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-03-16 20:24:04 +0100

    Added Javadoc for interface Player

--- src/main/java/it/unibo/jumpig/model/api/Player.java
+++ src/main/java/it/unibo/jumpig/model/api/Player.java
@@ -1,7 +1,9 @@
 package it.unibo.jumpig.model.api;
 
+/**
+ * An interface that represents the player piloted by the player in the game.
+ */
 public interface Player extends GameEntity {
-    
     /**
      * Getter of the player's lives.
      * @return the player's lives.
