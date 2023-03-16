1973424f0e9e8b41e5d5aa01933624b92a404ee7
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-03-16 20:16:18 +0100

    Added getLives() in Player

---
+++ src/main/java/it/unibo/jumpig/model/api/Player.java
@@ -0,0 +1,11 @@
+package it.unibo.jumpig.model.api;
+
+public interface Player extends GameEntity {
+    
+    /**
+     * Getter of the player's lives.
+     * @return the player's lives.
+     */
+    int getLives();
+
+}
