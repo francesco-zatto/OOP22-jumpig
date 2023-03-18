9e950c41f8b3e4f8cbc64ff1b4bbe1e638277eba
Alessandro <alessandro.verna@studio.unibo.it>
2023-03-18 18:30:24 +0100

    Method getScore() created

--- src/main/java/it/unibo/jumpig/model/api/Score.java
+++ src/main/java/it/unibo/jumpig/model/api/Score.java
@@ -4,4 +4,10 @@ package it.unibo.jumpig.model.api;
  * Interface score.
  */
 public interface Score {
+
+    /**
+     * Getter for the score.
+     * @return the score
+     */
+    int getScore();
 }
