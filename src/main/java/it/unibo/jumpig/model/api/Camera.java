0cf39e5eb508154949c19e05c239d4d2640d3844
Alessandro <alessandro.verna@studio.unibo.it>
2023-03-18 18:32:46 +0100

    Return type for getHeight() changed

--- src/main/java/it/unibo/jumpig/model/api/Camera.java
+++ src/main/java/it/unibo/jumpig/model/api/Camera.java
@@ -6,7 +6,7 @@ package it.unibo.jumpig.model.api;
 public interface Camera {
     /**
      * Getter that returns the camera's height.
-     * @return an Integer that represents the camera's height
+     * @return a value that represents the camera's height
      */
-    Integer getHeight();
+    int getHeight();
 }
