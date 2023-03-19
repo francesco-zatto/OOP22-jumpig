34db32ac0a1b192c7785db82db23a310fca50465
2L_gigi <luigi.li@studio.unibo.it>
2023-03-19 22:47:34 +0100

    Added start and close method

---
+++ src/main/java/it/unibo/jumpig/controller/api/Status.java
@@ -0,0 +1,20 @@
+package it.unibo.jumpig.controller.api;
+
+/*
+ * Interface Controller
+ */
+
+public interface Status {
+
+    /*
+     * This method start an interface
+     */
+    void start();
+
+    /*
+     * This method close an opened interface
+     */
+
+    void close();
+
+}
