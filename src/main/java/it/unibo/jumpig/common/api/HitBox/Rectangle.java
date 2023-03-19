4a63020875b11d42dbfc660c7017d088d431d804
Francesco <francesco.filippini8@studio.unibo.it>
2023-03-19 21:15:11 +0100

    Implemented first part of Rectangle

--- src/main/java/it/unibo/jumpig/common/api/HitBox/Rectangle.java
+++ src/main/java/it/unibo/jumpig/common/api/HitBox/Rectangle.java
@@ -5,4 +5,33 @@ package it.unibo.jumpig.common.api.hitbox;
  */
 
 public class Rectangle implements ShapeHitBox {
+
+    private double x;
+    private double y;
+    private double width;
+    private double height;
+
+    /**
+     * Constructor for the Rectangle.
+     * 
+     * @param x  the abscissa from which the Rectangle begins
+     * @param y  the ordinate from which the Rectangle begins
+     * @param width  the width of the Rectangle
+     * @param height  the height of the Rectangle
+     */
+    public Rectangle(final double x, final double y, final double width, final double height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    /**
+     * The method to compute the area of the Rectangle which is base multiplied by height.
+     * @return the computed area of the Rectangle
+     */
+    @Override
+    public double computeArea() {
+        return 0.55; //TODO
+    }
 }
