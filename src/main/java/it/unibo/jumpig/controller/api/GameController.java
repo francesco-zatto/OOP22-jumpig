4ca42b1ac45036b6f8027299beb7710436a0bbcb
2L_gigi <luigi.li@studio.unibo.it>
2023-03-19 22:48:23 +0100

    Added extends from Status

--- src/main/java/it/unibo/jumpig/controller/api/GameController.java
+++ src/main/java/it/unibo/jumpig/controller/api/GameController.java
@@ -3,7 +3,7 @@ package it.unibo.jumpig.controller.api;
 /**
  * Interface Game Controller
  */
-public interface GameController {
+public interface GameController extends Status {
     /**
      * This method register the input from the keyboard while in game
      */
