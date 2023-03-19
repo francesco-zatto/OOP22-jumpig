a53ea7c171a74316978fe23dfd5eec62485181a2
2L_gigi <luigi.li@studio.unibo.it>
2023-03-19 15:09:56 +0100

    Updated javadoc comments in Leaderboard controller

--- src/main/java/it/unibo/jumpig/controller/api/LeaderboardController.java
+++ src/main/java/it/unibo/jumpig/controller/api/LeaderboardController.java
@@ -1,9 +1,12 @@
 package it.unibo.jumpig.controller.api;
 
-/**
+/*
  * Interface Leaderboard Controller
  */
 
 public interface LeaderboardController {
+    /*
+     * Methods still to be definded
+     */
 
 }
