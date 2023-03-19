f49b4710d65b149cfa5248537844e35ed67f1017
Francesco <francesco.filippini8@studio.unibo.it>
2023-03-19 22:46:46 +0100

    Added methods getBounds and updateHitBox in HitBox

--- src/main/java/it/unibo/jumpig/common/api/HitBox/Hitbox.java
+++ src/main/java/it/unibo/jumpig/common/api/HitBox/Hitbox.java
@@ -1,5 +1,7 @@
 package it.unibo.jumpig.common.api.hitbox;
 
+import it.unibo.jumpig.common.api.Position;
+
 /**
  * The interface to manage the existence area(HitBox) of each game entity.
  */
@@ -11,4 +13,13 @@ public interface Hitbox {
      * @return the Rectangle that rapresents the HitBox
      */
     Rectangle getBounds();
+
+    /**
+     * The method to update the HitBox position.
+     * @param xy  the abscissa and the ordinate from which the Rectangle begins
+     * @param width  the width of the Rectangle
+     * @param height  the height of the Rectangle
+     * @return  the Rectangle created that is the updated HitBox
+     */
+    Rectangle updateHitBox(final Position xy, final double width, final double height);
 }
