c9bb4100177650784980920584f2373874cc1f1d
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-03-20 17:12:49 +0100

    Changed package for gameEntities

--- src/main/java/it/unibo/jumpig/model/api/Coin.java
+++
@@ -1,7 +0,0 @@
-package it.unibo.jumpig.model.api;
-
-/**
- * An interface that represents the coin which is collectible by the player.
- */
-public interface Coin extends GameEntity {
-}
--- src/main/java/it/unibo/jumpig/model/api/Enemy.java
+++
@@ -1,8 +0,0 @@
-package it.unibo.jumpig.model.api;
-
-/**
- * An interface that represents the enemy of the game, an entity which
- * the player should avoid, otherwise he will lose a life, or worse, die. 
- */
-public interface Enemy extends GameEntity {
-}
