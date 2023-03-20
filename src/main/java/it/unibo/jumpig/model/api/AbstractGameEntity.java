540a9a53a04997a65d005b69dd6c3347557e4d84
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-03-20 12:50:37 +0100

    Created AbstractGameEntity

---
+++ src/main/java/it/unibo/jumpig/model/api/AbstractGameEntity.java
@@ -0,0 +1,23 @@
+package it.unibo.jumpig.model.api;
+
+import it.unibo.jumpig.common.api.Position;
+import it.unibo.jumpig.common.api.hitbox.Hitbox;
+
+/**
+ * Class to manage the position and the hitbox of each gameEntity.
+*/
+public abstract class AbstractGameEntity implements GameEntity {
+
+    @Override
+    public Position getPosition() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPosition'");
+    }
+
+    @Override
+    public Hitbox getHitbox() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getHitbox'");
+    }
+    
+}
