548dced2c7d28f725ec438c978b62771a5d86874
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-03-21 11:00:47 +0100

    Added overridden methods in BasicPlatform

--- src/main/java/it/unibo/jumpig/model/impl/gameentity/BasicPlatform.java
+++ src/main/java/it/unibo/jumpig/model/impl/gameentity/BasicPlatform.java
@@ -1,5 +1,28 @@
 package it.unibo.jumpig.model.impl.gameentity;
 
-public class BasicPlatform {
+import it.unibo.jumpig.common.api.Position;
+import it.unibo.jumpig.common.api.hitbox.Hitbox;
+import it.unibo.jumpig.model.api.Velocity;
+import it.unibo.jumpig.model.api.gameentity.AbstractGameEntity;
+import it.unibo.jumpig.model.api.gameentity.Platform;
+
+public class BasicPlatform extends AbstractGameEntity implements Platform {
+
+    public BasicPlatform(Position position, Hitbox hitbox) {
+        super(position, hitbox);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public Velocity getJumpVelocity() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getJumpVelocity'");
+    }
+
+    @Override
+    public int getLength() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getLength'");
+    }
     
 }
