d6695c1e7b4d453710703b269ad3dcb19ea11ed2
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-03-22 20:54:13 +0100

    Added Javadoc in handle() in CollisionHandler

--- src/main/java/it/unibo/jumpig/model/api/CollisionHandler.java
+++ src/main/java/it/unibo/jumpig/model/api/CollisionHandler.java
@@ -8,7 +8,11 @@ import it.unibo.jumpig.model.api.gameentity.Player;
  * player and a gameEntity.
  */
 public interface CollisionHandler<E extends GameEntity> {
-
+    /**
+     * Method that handles a collision between player and a gameEntity
+     * @param player player that collided 
+     * @param gameEntity gameEntity which the player collided with
+     */
     void handle(Player player, E gameEntity);
     
 }
