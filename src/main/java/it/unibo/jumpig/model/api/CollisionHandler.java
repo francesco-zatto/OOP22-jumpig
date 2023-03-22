6230ae047db7aba25a3ae58ec3e84d72c45bb89e
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-03-22 20:50:59 +0100

    Added method handle() in CollisionHandler

--- src/main/java/it/unibo/jumpig/model/api/CollisionHandler.java
+++ src/main/java/it/unibo/jumpig/model/api/CollisionHandler.java
@@ -1,11 +1,14 @@
 package it.unibo.jumpig.model.api;
 
 import it.unibo.jumpig.model.api.gameentity.GameEntity;
+import it.unibo.jumpig.model.api.gameentity.Player;
 
 /**
  * Interface whose implementations have to handle collisions between
  * player and a gameEntity.
  */
-public class CollisionHandler<E extends GameEntity> {
+public interface CollisionHandler<E extends GameEntity> {
+
+    void handle(Player player, E gameEntity);
     
 }
