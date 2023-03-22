411819fdce6f450a7bbf3e5ac7a54123c0747ee0
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-03-22 20:49:30 +0100

    Created interface CollisionHandler

---
+++ src/main/java/it/unibo/jumpig/model/api/CollisionHandler.java
@@ -0,0 +1,11 @@
+package it.unibo.jumpig.model.api;
+
+import it.unibo.jumpig.model.api.gameentity.GameEntity;
+
+/**
+ * Interface whose implementations have to handle collisions between
+ * player and a gameEntity.
+ */
+public class CollisionHandler<E extends GameEntity> {
+    
+}
