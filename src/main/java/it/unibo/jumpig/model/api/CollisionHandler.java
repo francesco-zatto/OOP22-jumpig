679ebfe1ee4bd910d3dbc0741f990d2e96cd8747
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-03-22 21:01:14 +0100

    Added param <E> in Javadoc in CollisionHandler

--- src/main/java/it/unibo/jumpig/model/api/CollisionHandler.java
+++ src/main/java/it/unibo/jumpig/model/api/CollisionHandler.java
@@ -6,13 +6,13 @@ import it.unibo.jumpig.model.api.gameentity.Player;
 /**
  * Interface whose implementations have to handle collisions between
  * player and a gameEntity.
+ * @param <E> any kind of gameEntity that the player could collide with.
  */
 public interface CollisionHandler<E extends GameEntity> {
     /**
-     * Method that handles a collision between player and a gameEntity
-     * @param player player that collided 
-     * @param gameEntity gameEntity which the player collided with
+     * Method that handles a collision between player and a gameEntity.
+     * @param player player that collided.
+     * @param gameEntity gameEntity which the player collided with.
      */
     void handle(Player player, E gameEntity);
-    
 }
