2fec63a1f69c5742495d85475cafe8d1e664f3b8
Alessandro <alessandro.verna@studio.unibo.it>
2023-03-22 18:55:54 +0100

    Class VelocityImpl created

---
+++ src/main/java/it/unibo/jumpig/model/impl/VelocityImpl.java
@@ -0,0 +1,46 @@
+package it.unibo.jumpig.model.impl;
+
+import it.unibo.jumpig.common.api.Position;
+import it.unibo.jumpig.model.api.Velocity;
+
+/**
+ * Class VelocityImpl that describes and calculates the entity's velocity.
+ */
+public class VelocityImpl implements Velocity {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public double getModule() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getModule'");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public double getXComponent() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getXComponent'");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public double getYComponent() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getYComponent'");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Position computeMovement(final Position initialPosition, final double dt) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'computeMovement'");
+    }
+}
