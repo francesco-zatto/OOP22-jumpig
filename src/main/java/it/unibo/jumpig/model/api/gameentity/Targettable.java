7ed94821328b489b75018311e15b5b9c8b75a514
Francesco <francesco.filippini8@studio.unibo.it>
2023-03-23 15:36:00 +0100

    Added javadoc in method setTarget() in Targettable

--- src/main/java/it/unibo/jumpig/model/api/gameentity/Targettable.java
+++ src/main/java/it/unibo/jumpig/model/api/gameentity/Targettable.java
@@ -5,10 +5,16 @@ package it.unibo.jumpig.model.api.gameentity;
  */
 public interface Targettable {
 
+    /**
+     * The method to set the targettability of a targettable entity. 
+     * @param setTargettable is true if the entity has to be set as taken,i.e. it will disappear.
+     */
     void setTarget(boolean setTargettable);
+
     /**
      * The method to know if the targettable entity has to disappear.
      * @return true if the entity has to disappear.
      */
     boolean isTaken();
+
 }
