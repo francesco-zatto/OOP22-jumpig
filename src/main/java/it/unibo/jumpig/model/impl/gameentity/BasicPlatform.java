dc6abe23a4a269b09b3477999af8601311823155
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-03-23 19:16:39 +0100

    Removed equals in BasicPlatform

--- src/main/java/it/unibo/jumpig/model/impl/gameentity/BasicPlatform.java
+++ src/main/java/it/unibo/jumpig/model/impl/gameentity/BasicPlatform.java
@@ -41,31 +41,4 @@ public class BasicPlatform extends AbstractGameEntity implements Platform {
     public double getLength() {
         return this.length;
     }
-
-    //TODO sposta hashCode e equals in Abstract
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean equals(final Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final BasicPlatform other = (BasicPlatform) obj;
-        if (this.jumpVelocity == null) {
-            if (other.jumpVelocity != null) {
-                return false;
-            }
-        } else if (!this.jumpVelocity.equals(other.jumpVelocity)) {
-            return false;
-        }
-        return Double.doubleToLongBits(this.length) == Double.doubleToLongBits(other.length);
-    }
 }
