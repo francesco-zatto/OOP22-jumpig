2955d9957c418e570da721f709fb3ce2df38f638
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-03-23 15:13:04 +0100

    Added method check in CollisionChecker

--- src/main/java/it/unibo/jumpig/model/api/collision/CollisionChecker.java
+++ src/main/java/it/unibo/jumpig/model/api/collision/CollisionChecker.java
@@ -1,6 +1,7 @@
 package it.unibo.jumpig.model.api.collision;
 
 import it.unibo.jumpig.model.api.gameentity.GameEntity;
+import it.unibo.jumpig.model.api.gameentity.Player;
 
 /**
  * Functional interface that checks a possible collision of a player
@@ -8,5 +9,11 @@ import it.unibo.jumpig.model.api.gameentity.GameEntity;
  * @param <E> any kind of gameEntity that the player could collide with.
  */
 public interface CollisionChecker<E extends GameEntity> {
-    
+    /**
+     * Method that checks if the given player and gameEntity are colliding.
+     * @param player player which could be in collision.
+     * @param gameEntity gameEntity which the player could have collided with.
+     * @return true if there is a collision, otherwise false.
+     */
+    boolean check(Player player, E gameEntity);
 }
