379069e712d7ea72c61e612e20271f75065403a3
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-03-23 19:29:14 +0100

    Added hashCode and equals in AbstractGameEntity

--- src/main/java/it/unibo/jumpig/model/api/gameentity/AbstractGameEntity.java
+++ src/main/java/it/unibo/jumpig/model/api/gameentity/AbstractGameEntity.java
@@ -36,4 +36,48 @@ public abstract class AbstractGameEntity implements GameEntity {
     public final Hitbox getHitbox() {
         return this.hitbox;
     }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.position == null) ? 0 : this.position.hashCode());
+        result = prime * result + ((this.hitbox == null) ? 0 : this.hitbox.hashCode());
+        return result;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final AbstractGameEntity other = (AbstractGameEntity) obj;
+        if (this.position == null) {
+            if (other.position != null) {
+                return false;
+            }
+        } else if (!position.equals(other.position)) {
+            return false;
+        }
+        if (this.hitbox == null) {
+            if (other.hitbox != null) {
+                return false;
+            }
+        } else if (!this.hitbox.equals(other.hitbox)) {
+            return false;
+        }
+        return true;
+    }
 }
