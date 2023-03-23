a2960ef0044889eaf057a9e502bfcb31b890ddef
Francesco <francesco.filippini8@studio.unibo.it>
2023-03-23 16:21:51 +0100

    Changed return type in both methods of Hitbox

--- src/main/java/it/unibo/jumpig/common/api/hitbox/Hitbox.java
+++ src/main/java/it/unibo/jumpig/common/api/hitbox/Hitbox.java
@@ -6,20 +6,20 @@ import it.unibo.jumpig.common.api.Position;
  * The interface to manage the existence area(HitBox) of each game entity.
  */
 
-public interface Hitbox {
+public interface Hitbox<Shape extends ShapeHitbox> {
 
     /**
      * The method to get the HitBox in the form of a Rectangle.
-     * @return the Rectangle that rapresents the HitBox
+     * @return the shape that rapresents the HitBox
      */
-    Rectangle getBounds();
+    Shape getBounds();
 
     /**
      * The method to update the HitBox position.
      * @param xy  the abscissa and the ordinate from which the Rectangle begins
      * @param width  the width of the Rectangle
      * @param height  the height of the Rectangle
-     * @return  the Rectangle created that is the updated HitBox
+     * @return the shape created that is the updated HitBox
      */
-    Rectangle updateHitBox(Position xy, double width, double height);
+    Shape updateHitBox(Position xy, double width, double height);
 }
