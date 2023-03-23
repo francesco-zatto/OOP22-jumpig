428b162a80dcee967b4fb7f5d777e6d17ccc303c
Alessandro <alessandro.verna@studio.unibo.it>
2023-03-23 11:04:47 +0100

    Created class PositionImpl.java
    Implemented getters, created constructors and fields

---
+++ src/main/java/it/unibo/jumpig/common/impl/PositionImpl.java
@@ -0,0 +1,38 @@
+package it.unibo.jumpig.common.impl;
+
+import it.unibo.jumpig.common.api.Position;
+
+/**
+ * Class PositionImpl that implemets Position in a Cartesian plane.
+ */
+public class PositionImpl implements Position {
+
+    private final double x;
+    private final double y;
+
+    /**
+     * Constructor for the creation of a position.
+     * @param x the abscissa
+     * @param y the ordinate
+     */
+    public PositionImpl(final double x, final double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public double getX() {
+        return this.x;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public double getY() {
+        return this.y;
+    }
+}
