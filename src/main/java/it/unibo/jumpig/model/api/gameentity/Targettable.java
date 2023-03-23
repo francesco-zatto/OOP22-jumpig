ec9fc7b5eec0f0ff06014ffe211d97bfff0da5c0
Francesco <francesco.filippini8@studio.unibo.it>
2023-03-23 15:23:48 +0100

    Added javadoc in method isTaken() in Targettable

--- src/main/java/it/unibo/jumpig/model/api/gameentity/Targettable.java
+++ src/main/java/it/unibo/jumpig/model/api/gameentity/Targettable.java
@@ -1,8 +1,12 @@
 package it.unibo.jumpig.model.api.gameentity;
 /**
- * The interface to understand if a targettable entity should remain visible or not.
+ * The interface to understand if a targettable entity has to remain visible or not.
  * For example, if a coin has been taken it has to disappear.
  */
 public interface Targettable {
+    /**
+     * The method to know if the targettable entity has to disappear.
+     * @return true if the entity has to disappear.
+     */
     boolean isTaken();
 }
