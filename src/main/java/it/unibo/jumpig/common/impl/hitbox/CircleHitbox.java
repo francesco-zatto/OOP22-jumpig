7ffb59bc57f8d37689bb62516a74475d4c4750bb
Francesco <francesco.filippini8@studio.unibo.it>
2023-03-24 16:05:35 +0100

    Created class CircleHitbox

---
+++ src/main/java/it/unibo/jumpig/common/impl/hitbox/CircleHitbox.java
@@ -0,0 +1,11 @@
+package it.unibo.jumpig.common.impl.hitbox;
+
+import it.unibo.jumpig.common.api.hitbox.Hitbox;
+
+/**
+ * The class to manage a circular Hitbox.
+ */
+
+public class CircleHitbox implements Hitbox<Circle> {
+    
+}
