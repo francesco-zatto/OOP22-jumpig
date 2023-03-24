98a6a70bd4e4a9f5addcf83cac9146b385521090
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-03-24 16:13:28 +0100

    Added method createPlatformCollisionHandler in CollisionHandlerFactory

--- src/main/java/it/unibo/jumpig/model/api/collision/CollisionHandlerFactory.java
+++ src/main/java/it/unibo/jumpig/model/api/collision/CollisionHandlerFactory.java
@@ -1,8 +1,18 @@
 package it.unibo.jumpig.model.api.collision;
 
+import it.unibo.jumpig.common.impl.hitbox.Rectangle;
+import it.unibo.jumpig.common.impl.hitbox.RectangleHitbox;
+import it.unibo.jumpig.model.api.gameentity.Platform;
+
 /**
  * Interface that produces any kind of collisionHandler for any kind of
  * collision that can happen in the game.
  */
 public interface CollisionHandlerFactory {
+
+    /**
+     * Method that creates a collisionHandler for collisions with platforms.
+     * @return a collisionHandler to handle collisions with platforms
+     */
+    CollisionHandler<Rectangle, RectangleHitbox, Platform> createPlatformCollisionHandler();
 }
