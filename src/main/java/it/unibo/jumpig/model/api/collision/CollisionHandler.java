c4bf3a9c4a996946ccbcdbc96d4d5e1612fe056b
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-03-24 12:50:42 +0100

    Modified Javadoc in CollisionHandler

--- src/main/java/it/unibo/jumpig/model/api/collision/CollisionHandler.java
+++ src/main/java/it/unibo/jumpig/model/api/collision/CollisionHandler.java
@@ -4,13 +4,13 @@ import it.unibo.jumpig.model.api.gameentity.GameEntity;
 import it.unibo.jumpig.model.api.gameentity.Player;
 
 /**
- * Interface whose implementations have to handle collisions between
+ * Interface whose implementations have to handle possible collisions between
  * player and a gameEntity.
  * @param <E> any kind of gameEntity that the player could collide with.
  */
 public interface CollisionHandler<E extends GameEntity> {
     /**
-     * Method that handles a collision between player and a gameEntity.
+     * Method that handles a possible collision between player and a gameEntity.
      * @param player player that collided.
      * @param gameEntity gameEntity which the player collided with.
      */
