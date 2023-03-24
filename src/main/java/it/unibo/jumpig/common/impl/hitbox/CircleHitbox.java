2b405a304ad25f0feb968fa7bc7c4a93b617a107
Francesco <francesco.filippini8@studio.unibo.it>
2023-03-24 16:14:29 +0100

    Added javadoc in the constructor of CircleHitbox

--- src/main/java/it/unibo/jumpig/common/impl/hitbox/CircleHitbox.java
+++ src/main/java/it/unibo/jumpig/common/impl/hitbox/CircleHitbox.java
@@ -11,6 +11,12 @@ public class CircleHitbox implements Hitbox<Circle> {
 
     private final Circle circle;
 
+    /**
+     * The constructor to create a new circular Hitbox.
+     * @param center the center of the circular Hitbox.
+     * @param radius the radius of the circular Hitbox.
+     */
+    
     public CircleHitbox(final Position center, final double radius) {
         this.circle = new Circle(center, radius);
     }
