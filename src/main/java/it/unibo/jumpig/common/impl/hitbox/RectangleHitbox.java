5e95ea844bfc4c18afe4f6f85357a73176ce1236
Francesco <francesco.filippini8@studio.unibo.it>
2023-03-24 13:40:37 +0100

    Added javadoc for the constructor in RectangleHitbox

--- src/main/java/it/unibo/jumpig/common/impl/hitbox/RectangleHitbox.java
+++ src/main/java/it/unibo/jumpig/common/impl/hitbox/RectangleHitbox.java
@@ -11,9 +11,17 @@ public class RectangleHitbox implements Hitbox<Rectangle> {
 
     private final Rectangle rectangle;
 
+    /**
+     * The constructor to create a new rectangular Hitbox.
+     * @param position  the abscissa and the ordinate of the center of the rectangular Hitbox.
+     * @param width  the width of the rectangular Hitbox.
+     * @param height  the height of the rectangular Hitbox.
+     */
+    
     public RectangleHitbox(final Position position, final double width, final double height) {
         this.rectangle = new Rectangle(position, width, height);
     }
+
     /**
      * {@inheritDoc}
      */
