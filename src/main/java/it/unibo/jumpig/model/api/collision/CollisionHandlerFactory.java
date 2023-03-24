f20b61d875112e32440344290b944918c92e6521
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-03-24 16:04:38 +0100

    Created CollisionHandlerFactory

---
+++ src/main/java/it/unibo/jumpig/model/api/collision/CollisionHandlerFactory.java
@@ -0,0 +1,8 @@
+package it.unibo.jumpig.model.api.collision;
+
+/**
+ * Interface that produces any kind of collisionHandler for any kind of
+ * collision that can happen in the game.
+ */
+public interface CollisionHandlerFactory {
+}
