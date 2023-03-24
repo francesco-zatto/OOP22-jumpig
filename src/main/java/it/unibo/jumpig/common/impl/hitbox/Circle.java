32a5f362ff763b4757cefcb8d6f3261bb471487a
Francesco <francesco.filippini8@studio.unibo.it>
2023-03-24 15:12:33 +0100

    Added javadoc for Circle

--- src/main/java/it/unibo/jumpig/common/impl/hitbox/Circle.java
+++ src/main/java/it/unibo/jumpig/common/impl/hitbox/Circle.java
@@ -2,6 +2,10 @@ package it.unibo.jumpig.common.impl.hitbox;
 
 import it.unibo.jumpig.common.api.hitbox.ShapeHitbox;
 
+/**
+ * The class to manage the circular area of the Hitbox.
+ */
+
 public class Circle implements ShapeHitbox {
     
 }
