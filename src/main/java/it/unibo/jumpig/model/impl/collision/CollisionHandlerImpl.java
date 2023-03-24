4761054d19dafbb7ccd3e6a6342502a1d8436565
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-03-24 13:17:51 +0100

    Added fields and constructor in CollisionHandlerImpl

--- src/main/java/it/unibo/jumpig/model/impl/collision/CollisionHandlerImpl.java
+++ src/main/java/it/unibo/jumpig/model/impl/collision/CollisionHandlerImpl.java
@@ -1,5 +1,7 @@
 package it.unibo.jumpig.model.impl.collision;
 
+import it.unibo.jumpig.model.api.collision.CollisionActioner;
+import it.unibo.jumpig.model.api.collision.CollisionChecker;
 import it.unibo.jumpig.model.api.collision.CollisionHandler;
 import it.unibo.jumpig.model.api.gameentity.GameEntity;
 import it.unibo.jumpig.model.api.gameentity.Player;
@@ -9,6 +11,21 @@ import it.unibo.jumpig.model.api.gameentity.Player;
  * @param <E> any kind of gameEntity that the player could collide with.
  */
 public class CollisionHandlerImpl<E extends GameEntity> implements CollisionHandler<E> {
+
+    private final CollisionChecker<E> collisionChecker;
+    private final CollisionActioner<E> collisionActioner;
+
+    /**
+     * Constructor that creates a new CollisionHandler, which depends on the collisionChecker and
+     * on the collisionActioner.
+     * @param collisionChecker
+     * @param collisionActioner
+     */
+    public CollisionHandlerImpl(final CollisionChecker<E> collisionChecker, 
+            final CollisionActioner<E> collisionActioner) {
+        this.collisionChecker = collisionChecker;
+        this.collisionActioner = collisionActioner;
+    }
 
     /**
      * Method that, given a collisionChecker and a collisionActioner, checks a possible 
