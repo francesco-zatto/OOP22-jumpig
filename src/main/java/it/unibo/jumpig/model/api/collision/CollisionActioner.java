e7732bef8b6a04b215be8d931336661d80dc03f8
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-03-24 13:01:45 +0100

    Added method act in CollisionActioner

--- src/main/java/it/unibo/jumpig/model/api/collision/CollisionActioner.java
+++ src/main/java/it/unibo/jumpig/model/api/collision/CollisionActioner.java
@@ -1,6 +1,7 @@
 package it.unibo.jumpig.model.api.collision;
 
 import it.unibo.jumpig.model.api.gameentity.GameEntity;
+import it.unibo.jumpig.model.api.gameentity.Player;
 
 /**
  * Functional interface that acts in a specific way when a player and
@@ -8,4 +9,10 @@ import it.unibo.jumpig.model.api.gameentity.GameEntity;
  * @param <E> any kind of gameEntity that the player collided with
  */
 public interface CollisionActioner<E extends GameEntity> {
+    /**
+     * Method that acts on the player and the gameEntity collided.
+     * @param player player which collided.
+     * @param gameEntity gameEntity which the player collided with.
+     */
+    void act(Player player, E gameEntity);
 }
