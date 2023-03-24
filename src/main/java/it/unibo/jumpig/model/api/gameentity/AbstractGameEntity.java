2955c11f87f358f4b30c96cc1acceb371fbaaf5f
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-03-24 15:50:08 +0100

    Added type-variables S and H in AbstractGameEntity

--- src/main/java/it/unibo/jumpig/model/api/gameentity/AbstractGameEntity.java
+++ src/main/java/it/unibo/jumpig/model/api/gameentity/AbstractGameEntity.java
@@ -2,21 +2,24 @@ package it.unibo.jumpig.model.api.gameentity;
 
 import it.unibo.jumpig.common.api.Position;
 import it.unibo.jumpig.common.api.hitbox.Hitbox;
+import it.unibo.jumpig.common.api.hitbox.ShapeHitbox;
 
 /**
  * Class to manage the position and the hitbox of each gameEntity.
+ * @param <S> any kind of ShapeHitbox
+ * @param <H> any kind of Hitbox
 */
-public abstract class AbstractGameEntity implements GameEntity {
+public abstract class AbstractGameEntity<S extends ShapeHitbox, H extends Hitbox<S>> implements GameEntity<S, H> {
 
     private final Position position;
-    private final Hitbox hitbox;
+    private final H hitbox;
 
     /**
      * Constructor for any gameEntity.
      * @param position position of the gameEntity in the world.
      * @param hitbox hitbox of the gameEntity.
      */
-    public AbstractGameEntity(final Position position, final Hitbox hitbox) {
+    public AbstractGameEntity(final Position position, final H hitbox) {
         this.position = position;
         this.hitbox = hitbox;
     }
@@ -33,7 +36,7 @@ public abstract class AbstractGameEntity implements GameEntity {
      * {@inheritDoc}
      */
     @Override
-    public final Hitbox getHitbox() {
+    public final H getHitbox() {
         return this.hitbox;
     }
 
@@ -44,8 +47,8 @@ public abstract class AbstractGameEntity implements GameEntity {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((this.position == null) ? 0 : this.position.hashCode());
-        result = prime * result + ((this.hitbox == null) ? 0 : this.hitbox.hashCode());
+        result = prime * result + ((position == null) ? 0 : position.hashCode());
+        result = prime * result + ((hitbox == null) ? 0 : hitbox.hashCode());
         return result;
     }
 
@@ -63,19 +66,19 @@ public abstract class AbstractGameEntity implements GameEntity {
         if (getClass() != obj.getClass()) {
             return false;
         }
-        final AbstractGameEntity other = (AbstractGameEntity) obj;
-        if (this.position == null) {
+        AbstractGameEntity<S, H> other = (AbstractGameEntity<S, H>) obj; //NOPMD
+        if (position == null) {
             if (other.position != null) {
                 return false;
             }
         } else if (!position.equals(other.position)) {
             return false;
         }
-        if (this.hitbox == null) {
+        if (hitbox == null) {
             if (other.hitbox != null) {
                 return false;
             }
-        } else if (!this.hitbox.equals(other.hitbox)) {
+        } else if (!hitbox.equals(other.hitbox)) {
             return false;
         }
         return true;
