758a33685bbf65b349d84400ea21d4d68be85e1a
Francesco <francesco.filippini8@studio.unibo.it>
2023-03-24 12:48:14 +0100

    Added javadoc comment in RectangleHitbox

--- src/main/java/it/unibo/jumpig/common/impl/hitbox/RectangleHitbox.java
+++ src/main/java/it/unibo/jumpig/common/impl/hitbox/RectangleHitbox.java
@@ -1,7 +1,12 @@
 package it.unibo.jumpig.common.impl.hitbox;
 
+import it.unibo.jumpig.common.api.Position;
 import it.unibo.jumpig.common.api.hitbox.Hitbox;
 
+/**
+ * The class to manage a rectangular Hitbox.
+ */
+
 public class RectangleHitbox implements Hitbox<Rectangle> {
-    
+
 }
