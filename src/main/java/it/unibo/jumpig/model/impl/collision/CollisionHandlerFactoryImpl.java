d41b2607886bb798db8c21e982cb437120fcc9b3
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-03-26 10:54:22 +0200

    Overriden createEnemyCollisionHandler in impl

--- src/main/java/it/unibo/jumpig/model/impl/collision/CollisionHandlerFactoryImpl.java
+++ src/main/java/it/unibo/jumpig/model/impl/collision/CollisionHandlerFactoryImpl.java
@@ -6,6 +6,7 @@ import it.unibo.jumpig.common.impl.hitbox.Rectangle;
 import it.unibo.jumpig.common.impl.hitbox.RectangleHitbox;
 import it.unibo.jumpig.model.api.collision.CollisionHandler;
 import it.unibo.jumpig.model.api.collision.CollisionHandlerFactory;
+import it.unibo.jumpig.model.api.gameentity.Enemy;
 import it.unibo.jumpig.model.api.gameentity.Platform;
 import it.unibo.jumpig.model.api.gameentity.Player;
 
@@ -75,5 +76,14 @@ public class CollisionHandlerFactoryImpl implements CollisionHandlerFactory {
 
     private void playerJumps(final Player player, final Platform platform) {
         player.setVelocityFromJump(platform.getJumpVelocity());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public CollisionHandler<Rectangle, RectangleHitbox, Enemy> createEnemyCollisionHandler() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createEnemyCollisionHandler'");
     }
 }
