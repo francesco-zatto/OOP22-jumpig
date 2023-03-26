764c132e2c356c937125d4d86e5e77ed582854c1
Francesco <francesco.filippini8@studio.unibo.it>
2023-03-26 21:03:48 +0200

    Added javadoc in BasicCoin

---
+++ src/main/java/it/unibo/jumpig/model/impl/gameentity/BasicCoin.java
@@ -0,0 +1,11 @@
+package it.unibo.jumpig.model.impl.gameentity;
+
+import it.unibo.jumpig.model.api.gameentity.Coin;
+
+/**
+ * The class that represents a basic coin.
+ * It has always the same value and once taken by the player it disappears.
+ */
+public class BasicCoin implements Coin {
+    
+}
