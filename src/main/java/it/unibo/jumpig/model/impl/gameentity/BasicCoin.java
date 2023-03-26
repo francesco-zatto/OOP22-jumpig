ef0283163fe79ecb686d856fda5ea18f42743dfb
Francesco <francesco.filippini8@studio.unibo.it>
2023-03-26 21:09:47 +0200

    Added constructor for BasicCoin

--- src/main/java/it/unibo/jumpig/model/impl/gameentity/BasicCoin.java
+++ src/main/java/it/unibo/jumpig/model/impl/gameentity/BasicCoin.java
@@ -1,14 +1,20 @@
 package it.unibo.jumpig.model.impl.gameentity;
 
 import it.unibo.jumpig.common.api.Position;
+import it.unibo.jumpig.common.impl.hitbox.Circle;
 import it.unibo.jumpig.common.impl.hitbox.CircleHitbox;
+import it.unibo.jumpig.model.api.gameentity.AbstractGameEntity;
 import it.unibo.jumpig.model.api.gameentity.Coin;
 
 /**
  * The class that represents a basic coin.
  * It has always the same value and once taken by the player it disappears.
  */
-public class BasicCoin implements Coin {
+public class BasicCoin extends AbstractGameEntity<Circle,CircleHitbox> implements Coin {
+
+    public BasicCoin(Position position, CircleHitbox hitbox) {
+        super(position, hitbox);
+    }
 
     @Override
     public Position getPosition() {
@@ -33,5 +39,4 @@ public class BasicCoin implements Coin {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'isTaken'");
     }
-    
 }
