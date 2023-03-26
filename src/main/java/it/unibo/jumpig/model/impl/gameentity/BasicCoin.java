cb966da894163dda13eca7341cd298ef092bd292
Francesco <francesco.filippini8@studio.unibo.it>
2023-03-26 21:04:23 +0200

    Added unimplemented methods in BasicCoin

--- src/main/java/it/unibo/jumpig/model/impl/gameentity/BasicCoin.java
+++ src/main/java/it/unibo/jumpig/model/impl/gameentity/BasicCoin.java
@@ -1,5 +1,7 @@
 package it.unibo.jumpig.model.impl.gameentity;
 
+import it.unibo.jumpig.common.api.Position;
+import it.unibo.jumpig.common.impl.hitbox.CircleHitbox;
 import it.unibo.jumpig.model.api.gameentity.Coin;
 
 /**
@@ -7,5 +9,29 @@ import it.unibo.jumpig.model.api.gameentity.Coin;
  * It has always the same value and once taken by the player it disappears.
  */
 public class BasicCoin implements Coin {
+
+    @Override
+    public Position getPosition() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPosition'");
+    }
+
+    @Override
+    public CircleHitbox getHitbox() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getHitbox'");
+    }
+
+    @Override
+    public void setTarget(boolean setTargettable) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setTarget'");
+    }
+
+    @Override
+    public boolean isTaken() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isTaken'");
+    }
     
 }
