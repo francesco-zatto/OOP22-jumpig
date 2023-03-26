766609dfb8b9364361c19a1a9a06ed77fba32e60
Francesco <francesco.filippini8@studio.unibo.it>
2023-03-26 22:44:29 +0200

    Implemented method updateHitbox in RectangleHitbox

--- src/main/java/it/unibo/jumpig/common/impl/hitbox/RectangleHitbox.java
+++ src/main/java/it/unibo/jumpig/common/impl/hitbox/RectangleHitbox.java
@@ -9,19 +9,19 @@ import it.unibo.jumpig.common.api.hitbox.Hitbox;
 
 public class RectangleHitbox implements Hitbox {
 
-    private final Position position;
+    private Position center;
     private final double width;
     private final double height;
 
     /**
      * The constructor to create a new rectangular Hitbox.
-     * @param position  the abscissa and the ordinate of the center of the rectangular Hitbox.
+     * @param center  the abscissa and the ordinate of the center of the rectangular Hitbox.
      * @param width  the width of the rectangular Hitbox.
      * @param height  the height of the rectangular Hitbox.
      */
 
-    public RectangleHitbox(final Position position, final double width, final double height) {
-        this.position = position;
+    public RectangleHitbox(final Position center, final double width, final double height) {
+        this.center = center;
         this.width = width;
         this.height = height;
     }
@@ -30,8 +30,8 @@ public class RectangleHitbox implements Hitbox {
      * {@inheritDoc}
      */
     @Override
-    public Rectangle updateHitBox(final Position center) {
-        return new Rectangle(center, this.rectangle.getWidth(), this.rectangle.getHeight());
+    public void updateHitBox(final Position center) {
+        this.center = center;
     }
 
 }
