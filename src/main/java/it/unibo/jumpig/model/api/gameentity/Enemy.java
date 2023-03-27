1ee6c10a5ddf4f974a4ccaa529a9c2bfa38c12bc
2L_gigi <luigi.li@studio.unibo.it>
2023-03-27 13:09:10 +0200

    Add new method and optimization

--- src/main/java/it/unibo/jumpig/model/api/gameentity/Enemy.java
+++ src/main/java/it/unibo/jumpig/model/api/gameentity/Enemy.java
@@ -1,8 +1,19 @@
 package it.unibo.jumpig.model.api.gameentity;
 
+import it.unibo.jumpig.common.impl.hitbox.RectangleHitbox;
+import it.unibo.jumpig.model.api.Velocity;
+
 /**
  * An interface that represents the enemy of the game, an entity which
- * the player should avoid, otherwise he will lose a life, or worse, die. 
+ * the player should avoid, otherwise he will lose a life, or worse, die.
  */
-public interface Enemy extends GameEntity {
+public interface Enemy extends GameEntity<RectangleHitbox>, Targettable {
+
+    /**
+     * This method should set a different velocity for each types of the enemies.
+     * 
+     * @param velocity is the enemies velocity based from the difficult.
+     */
+    Velocity setVelocity(Velocity velocity);
+
 }
