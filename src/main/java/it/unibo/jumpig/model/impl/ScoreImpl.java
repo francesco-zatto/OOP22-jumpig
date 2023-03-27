e5d56d558e760ec5bafeb2fa5cd66590a6e93c37
Alessandro <alessandro.verna@studio.unibo.it>
2023-03-27 17:34:40 +0200

    Created class ScoreImpl

---
+++ src/main/java/it/unibo/jumpig/model/impl/ScoreImpl.java
@@ -0,0 +1,42 @@
+package it.unibo.jumpig.model.impl;
+
+import it.unibo.jumpig.model.api.Score;
+
+/**
+ * Class ScoreImpl that creates a game's final score.
+ */
+public class ScoreImpl implements Score {
+
+    /**
+     * Constructor for the Score.
+     */
+    public ScoreImpl() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getUsername() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getUsername'");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getScore() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getScore'");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getCoins() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCoins'");
+    }
+}
