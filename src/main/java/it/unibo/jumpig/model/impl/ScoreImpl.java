f2754431d4e79413a7d1eee1644c1d443b795be9
Alessandro <alessandro.verna@studio.unibo.it>
2023-03-27 17:42:52 +0200

    Created fields and constructor

--- src/main/java/it/unibo/jumpig/model/impl/ScoreImpl.java
+++ src/main/java/it/unibo/jumpig/model/impl/ScoreImpl.java
@@ -6,11 +6,21 @@ import it.unibo.jumpig.model.api.Score;
  * Class ScoreImpl that creates a game's final score.
  */
 public class ScoreImpl implements Score {
-    
+
+    private final String username;
+    private final double score;
+    private final int coins;
+
     /**
-     * Constructor for the Score.
+     * Constructor for the score.
+     * @param username the player's username
+     * @param score the final score, i.e. the maximum height reached
+     * @param coins the amount of coins collected in a game
      */
-    public ScoreImpl() {
+    public ScoreImpl(final String username, final double score, final int coins) {
+        this.username = username;
+        this.score = score;
+        this.coins = coins;
     }
 
     /**
