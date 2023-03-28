880cd205b08c954c7d8cee324ccb3d993e18238c
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-03-28 14:11:56 +0200

    Created PlatformHitbox

---
+++ src/main/java/it/unibo/jumpig/common/impl/hitbox/PlatformHitbox.java
@@ -0,0 +1,20 @@
+package it.unibo.jumpig.common.impl.hitbox;
+
+import it.unibo.jumpig.common.api.Position;
+
+/**
+ * Class that represent a platform's hitbox.
+ */
+public class PlatformHitbox extends RectangleHitbox {
+
+    private static final double PLATFORM_WIDTH = 2; //TODO
+    private static final double PLATFORM_HEIGHT = 1; //TODO
+
+    /**
+     * Constructor for the platform's hitbox.
+     * @param center center of the hitbox
+    */
+    public PlatformHitbox(final Position center) {
+        super(center, PLATFORM_WIDTH, PLATFORM_HEIGHT);
+    }
+}
