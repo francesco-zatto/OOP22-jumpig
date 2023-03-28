386ed6c1f2afe3d96dcd891f626902e53af67801
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-03-28 18:01:56 +0200

    Created junit test class CollisionHandlerTest

---
+++ src/test/java/collision/CollisionHandlerTest.java
@@ -0,0 +1,9 @@
+package collision;
+
+/**
+ * Class to test collisionHandlers correctness.
+ * {@link it.unibo.jumpig.model.api.collision.CollisionHandler} 
+ * {@link it.unibo.jumpig.model.api.collision.CollisionHandlerFactory}
+ */
+public class CollisionHandlerTest {
+}
