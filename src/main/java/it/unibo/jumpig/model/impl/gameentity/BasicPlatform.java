0aa5f76aaaef34f5aa84c1241319aee3b060fbd9
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-03-28 14:13:32 +0200

    Changed BasicPlatform's constructor deleting parameter hitbox

--- src/main/java/it/unibo/jumpig/model/impl/gameentity/BasicPlatform.java
+++ src/main/java/it/unibo/jumpig/model/impl/gameentity/BasicPlatform.java
@@ -1,6 +1,7 @@
 package it.unibo.jumpig.model.impl.gameentity;
 
 import it.unibo.jumpig.common.api.Position;
+import it.unibo.jumpig.common.impl.hitbox.PlatformHitbox;
 import it.unibo.jumpig.common.impl.hitbox.RectangleHitbox;
 import it.unibo.jumpig.model.api.Velocity;
 import it.unibo.jumpig.model.api.gameentity.AbstractGameEntity;
@@ -18,11 +19,10 @@ public class BasicPlatform extends AbstractGameEntity<RectangleHitbox> implement
     /**
      * Constructor for a basic platform.
      * @param position position of the platform in the world.
-     * @param hitbox hitbox of the platform.
      * @param verticalJumpVelocity vertical velocity of a player when jumps on the platform. 
     */
-    public BasicPlatform(final Position position, final RectangleHitbox hitbox, final double verticalJumpVelocity) {
-        super(position, hitbox);
+    public BasicPlatform(final Position position, final double verticalJumpVelocity) {
+        super(position, new PlatformHitbox(position));
         this.jumpVelocity = new VelocityImpl(0, verticalJumpVelocity);
     }
 
