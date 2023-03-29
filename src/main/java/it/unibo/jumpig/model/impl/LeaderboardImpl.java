9ea07335cae96312d8221387d8943dab79af1c88
Alessandro <alessandro.verna@studio.unibo.it>
2023-03-29 13:59:34 +0200

    Created class LeaderboardImpl

---
+++ src/main/java/it/unibo/jumpig/model/impl/LeaderboardImpl.java
@@ -0,0 +1,22 @@
+package it.unibo.jumpig.model.impl;
+
+import java.util.List;
+
+import it.unibo.jumpig.model.api.Leaderboard;
+import it.unibo.jumpig.model.api.Score;
+
+public class LeaderboardImpl implements Leaderboard {
+
+    @Override
+    public List<Score> getScores() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getScores'");
+    }
+
+    @Override
+    public void addScore(Score score) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addScore'");
+    }
+    
+}
