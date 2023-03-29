d7855e7d70301dc5f5d568a6fae28c22719c7378
Alessandro <alessandro.verna@studio.unibo.it>
2023-03-29 19:27:03 +0200

    Created class PlayerHitbox

---
+++ src/main/java/it/unibo/jumpig/common/impl/hitbox/PlayerHitbox.java
@@ -0,0 +1,20 @@
+package it.unibo.jumpig.common.impl.hitbox;
+
+import it.unibo.jumpig.common.api.Position;
+
+/**
+ * Class that represents the player's hitbox.
+ */
+public class PlayerHitbox extends RectangleHitbox {
+
+    /**
+     * Constructor for the player's hitbox.
+     * @param center //TODO
+     * @param width //TODO
+     * @param height //TODO
+     */
+    public PlayerHitbox(final Position center, final double width, final double height) {
+        super(center, width, height);
+        //TODO Auto-generated constructor stub
+    } 
+}
