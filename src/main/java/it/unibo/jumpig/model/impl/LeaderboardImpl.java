47072d5102d4087e2315444e844632668284be9f
Alessandro <alessandro.verna@studio.unibo.it>
2023-03-29 14:09:26 +0200

    Added javadoc

--- src/main/java/it/unibo/jumpig/model/impl/LeaderboardImpl.java
+++ src/main/java/it/unibo/jumpig/model/impl/LeaderboardImpl.java
@@ -6,10 +6,12 @@ import java.util.List;
 import it.unibo.jumpig.model.api.Leaderboard;
 import it.unibo.jumpig.model.api.Score;
 
+/**
+ * Class that creates the leaderboard for the game.
+ */
 public class LeaderboardImpl implements Leaderboard {
 
     private List<Score> scoreLeaderboard;
-   
     /**
      * Constructor for the leaderboard.
      */
@@ -17,15 +19,20 @@ public class LeaderboardImpl implements Leaderboard {
         this.scoreLeaderboard = new ArrayList<>();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public List<Score> getScores() {
         return this.scoreLeaderboard;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void addScore(Score score) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'addScore'");
     }
-    
 }
