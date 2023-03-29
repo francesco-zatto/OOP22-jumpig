2ef96de4285930a7b8166b11a188a2bf66e1e9f7
2L_gigi <luigi.li@studio.unibo.it>
2023-03-29 22:30:13 +0200

    fixed few error

--- src/main/java/it/unibo/jumpig/model/impl/LeaderboardLoaderImpl.java
+++ src/main/java/it/unibo/jumpig/model/impl/LeaderboardLoaderImpl.java
@@ -3,38 +3,22 @@ package it.unibo.jumpig.model.impl;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import it.unibo.jumpig.model.api.Score;
 import it.unibo.jumpig.model.api.LeaderboardLoader;
 
 /**
- * This class LeaderboardLoaderImpl should order the list of scores and save.
+ * class LeaderboardLoaderImpl should order the list of scores and save them
+ * in the Leaderboard.txt.
  */
+
 public class LeaderboardLoaderImpl implements LeaderboardLoader {
-
-    /**
-     * This method take the correct separator for each different OS.
-     */
-    public static final String PROP_FILE_SEPARATOR = "file.separator";
-    String separator = System.getProperty(PROP_FILE_SEPARATOR);
-
-    /**
-     * This method saves the file countains the score.
-     * 
-     * @return file path
-     */
-    public String fileName() {
-        return ".." + separator + ".." + separator + ".." + separator + ".." + separator + ".."
-                + separator + ".." + separator + "Leaderboard.txt";
-    }
 
     /**
      * This method order the list of scores by sort.
@@ -55,7 +39,7 @@ public class LeaderboardLoaderImpl implements LeaderboardLoader {
     public void saveScores(final List<Score> sortedScore) {
         final Logger logger = Logger.getLogger(LeaderboardLoaderImpl.class.getName());
 
-        final File file = new File(fileName());
+        final File file = new File("./Leaderboard.txt");
         try {
             if (!file.createNewFile()) {
                 throw new IOException("Failed to create file");
@@ -64,32 +48,15 @@ public class LeaderboardLoaderImpl implements LeaderboardLoader {
             logger.log(Level.SEVERE, "An error occurred in creation of the file ", e);
         }
 
-        try (ObjectOutputStream outputStream = new ObjectOutputStream(
-                new FileOutputStream(fileName()))) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("./Leaderboard.txt", StandardCharsets.UTF_8))) {
             final List<Score> listScores = new ArrayList<>(sortedScore);
-            outputStream.writeObject(listScores);
+            for (final Score i : listScores) {
+                writer.write(i + "\n");
+            }
+            writer.close();
         } catch (IOException e) {
             logger.log(Level.SEVERE, "An error occurred while writing to file ", e);
-        }
 
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-
-    public void loadScores(final String file) {
-
-        try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(fileName()))) {
-
-            final ObjectInputStream in = new ObjectInputStream(new FileInputStream(fileName()));
-
-            in.close();
-
-        } catch (IOException e) {
-            Logger.getLogger(LeaderboardLoader.class.getName()).log(Level.SEVERE,
-                    "An error occurred while reading from file " + fileName(), e);
         }
 
     }
