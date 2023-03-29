369f7b2647883811f30a1c851d50d83e8a504fb6
Alessandro <alessandro.verna@studio.unibo.it>
2023-03-29 14:04:14 +0200

    Created constructor and field

--- src/main/java/it/unibo/jumpig/model/impl/LeaderboardImpl.java
+++ src/main/java/it/unibo/jumpig/model/impl/LeaderboardImpl.java
@@ -1,11 +1,21 @@
 package it.unibo.jumpig.model.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import it.unibo.jumpig.model.api.Leaderboard;
 import it.unibo.jumpig.model.api.Score;
 
 public class LeaderboardImpl implements Leaderboard {
+
+    private List<Score> scoreLeaderboard;
+   
+    /**
+     * Constructor for the leaderboard.
+     */
+    public LeaderboardImpl() {
+        this.scoreLeaderboard = new ArrayList<>();
+    }
 
     @Override
     public List<Score> getScores() {
