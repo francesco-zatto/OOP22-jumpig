2c0dcec53b08430a5cd540e95c8ef57437f21a9e
Alessandro <alessandro.verna@studio.unibo.it>
2023-03-30 12:30:50 +0200

    Created field frame and constructor

--- src/main/java/it/unibo/jumpig/view/impl/MenuViewSceneImpl.java
+++ src/main/java/it/unibo/jumpig/view/impl/MenuViewSceneImpl.java
@@ -1,11 +1,22 @@
 package it.unibo.jumpig.view.impl;
 
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
 import it.unibo.jumpig.view.api.MenuViewScene;
 
 /**
  * Class that displays the game's main menu.
  */
 public class MenuViewSceneImpl implements MenuViewScene {
+
+    private JFrame frame = new JFrame("Jumpig");
+    /**
+     * Constructor for the main menu.
+     */
+    public MenuViewSceneImpl() { 
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
 
     /**
      * {@inheritDoc}
