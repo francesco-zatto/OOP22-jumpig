f74d73ec924a6144ff890a3adddf6f2ee6cb0180
Alessandro <alessandro.verna@studio.unibo.it>
2023-03-30 17:18:33 +0200

    Create fields for scalable screen size

--- src/main/java/it/unibo/jumpig/view/impl/MenuViewSceneImpl.java
+++ src/main/java/it/unibo/jumpig/view/impl/MenuViewSceneImpl.java
@@ -1,5 +1,8 @@
 package it.unibo.jumpig.view.impl;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
@@ -12,11 +15,18 @@ public class MenuViewSceneImpl implements MenuViewScene {
 
     private static final String FRAME_TITLE = "Jumpig";
     private final JFrame frame = new JFrame(FRAME_TITLE);
+    private final Dimension screen;
+    private final int screenWidth;
+    private final int screenHeight;
     /**
      * Constructor for the main menu.
      */
     public MenuViewSceneImpl() { 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        screen = Toolkit.getDefaultToolkit().getScreenSize();
+        screenWidth = (int) screen.getWidth();
+        screenHeight = (int) screen.getHeight();
+        frame.setSize(screenWidth / 2, screenHeight / 2);
     }
 
     /**
