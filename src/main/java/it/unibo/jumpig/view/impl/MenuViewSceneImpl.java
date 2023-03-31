c5124b857aceb74050cb01245f50121d25a16caf
Alessandro <alessandro.verna@studio.unibo.it>
2023-03-31 16:55:00 +0200

    Implemented functions for importing images

--- src/main/java/it/unibo/jumpig/view/impl/MenuViewSceneImpl.java
+++ src/main/java/it/unibo/jumpig/view/impl/MenuViewSceneImpl.java
@@ -7,11 +7,19 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagLayout;
 import java.awt.Toolkit;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Objects;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
@@ -24,14 +32,37 @@ public class MenuViewSceneImpl implements MenuViewScene {
 
     private static final String FRAME_TITLE = "Jumpig";
     private final JFrame frame = new JFrame(FRAME_TITLE);
+    private static final String ROOT = "resources/";
     /**
      * Constructor for the main menu.
+     * @throws IOException
      */
-    public MenuViewSceneImpl() {
+    public MenuViewSceneImpl() throws IOException {
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
         final JPanel panelButton = new JPanel();
         panelButton.setBackground(Color.CYAN);
         panelButton.setLayout(new BoxLayout(panelButton, Y_AXIS));
+
+        final InputStream in = Objects.requireNonNull(
+            ClassLoader.getSystemResourceAsStream("settings/settings")
+        );
+        String line;
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
+            line = br.readLine();
+        }
+        final JLabel lab2 = new JLabel(line);
+        /*
+         * Loading of icons and images is made very easy!
+         */
+        final URL imgURL = ClassLoader.getSystemResource("images/jumpigIcon.png");
+        final ImageIcon icon = new ImageIcon(imgURL);
+        /*
+         * From now on, it's just plain GUI construction
+         */
+        final JLabel lab1 = new JLabel(icon);
+
+
         JButton gameButton = new JButton("START GAME");
         panelButton.add(gameButton);
         panelButton.add(Box.createRigidArea(new Dimension(0,10)));
@@ -39,10 +70,16 @@ public class MenuViewSceneImpl implements MenuViewScene {
         panelButton.add(leaderboardButton);
         panelButton.add(Box.createRigidArea(new Dimension(0,10)));
         panelButton.add(new JButton("QUIT"));
+
         final JPanel menuPanel = new JPanel(new GridBagLayout());
         menuPanel.setBackground(Color.CYAN);
-        menuPanel.add(panelButton);
+        
+        menuPanel.add(lab1);
+        menuPanel.add(lab2);
+        lab1.setSize(10,10);
+        menuPanel.repaint();
         frame.getContentPane().add(menuPanel, BorderLayout.CENTER);
+
         final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
         final int screenHeight = (int) screen.getHeight();
         final double percentageHeight = 0.7;
@@ -74,7 +111,7 @@ public class MenuViewSceneImpl implements MenuViewScene {
      * Main to test the GUI menu.
      * @param args strings
      */
-    public static void main(final String[] args) {
+    public static void main(final String[] args) throws Exception {
         new MenuViewSceneImpl().show();
     }
 }
