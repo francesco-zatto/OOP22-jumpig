beff51d56480097ef748b87f86340124b4d7bcdc
Alessandro <alessandro.verna@studio.unibo.it>
2023-04-02 21:07:29 +0200

    created class MenuControllerImpl

---
+++ src/main/java/it/unibo/jumpig/controller/impl/MenuControllerImpl.java
@@ -0,0 +1,46 @@
+package it.unibo.jumpig.controller.impl;
+
+import it.unibo.jumpig.controller.api.MenuController;
+
+/**
+ * Class that manages the menu interactions.
+ */
+public class MenuControllerImpl implements MenuController {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void start() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void close() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'close'");
+    }
+
+    /**
+     *{@inheritDoc}
+     */
+    @Override
+    public void notifyStartGame() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'notifyStartGame'");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void notifyStartLeaderboard() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'notifyStartLeaderboard'");
+    }
+    
+}
