a08f7e4bc7bd66bceb2f64f1062f676f89c1cf5e
Alessandro <alessandro.verna@studio.unibo.it>
2023-04-02 14:23:14 +0200

    removed resources used

--- src/main/java/it/unibo/jumpig/view/impl/MenuViewSceneImpl.java
+++ src/main/java/it/unibo/jumpig/view/impl/MenuViewSceneImpl.java
@@ -7,19 +7,10 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagLayout;
 import java.awt.Toolkit;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.Objects;
-
 import javax.swing.Box;
 import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
@@ -36,28 +27,11 @@ public class MenuViewSceneImpl implements MenuViewScene {
      * Constructor for the main menu.
      * @throws IOException
      */
-    public MenuViewSceneImpl() throws IOException {
+    public MenuViewSceneImpl() {
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         final JPanel panelButton = new JPanel();
         panelButton.setBackground(Color.CYAN);
         panelButton.setLayout(new BoxLayout(panelButton, Y_AXIS));
-        final InputStream in = Objects.requireNonNull(
-            ClassLoader.getSystemResourceAsStream("settings/settings")
-        );
-        String line;
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
-            line = br.readLine();
-        }
-        final JLabel lab2 = new JLabel(line);
-        /*
-         * Loading of icons and images is made very easy!
-         */
-        final URL imgURL = ClassLoader.getSystemResource("images/jumpigIcon.png");
-        final ImageIcon icon = new ImageIcon(imgURL);
-        /*
-         * From now on, it's just plain GUI construction
-         */
-        final JLabel lab1 = new JLabel(icon);
         JButton gameButton = new JButton("START GAME");
         panelButton.add(gameButton);
         panelButton.add(Box.createRigidArea(new Dimension(0,10)));
@@ -100,7 +74,7 @@ public class MenuViewSceneImpl implements MenuViewScene {
      * Main to test the GUI menu.
      * @param args strings
      */
-    public static void main(final String[] args) throws Exception {
+    public static void main(final String[] args) {
         new MenuViewSceneImpl().show();
     }
 }
