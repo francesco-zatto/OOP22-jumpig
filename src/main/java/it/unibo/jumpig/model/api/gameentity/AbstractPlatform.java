0091172e7b968b4b5a5d4f7faedf955ab3d9300d
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-04-03 22:38:35 +0200

    Added javadoc in AsbtractPlatform

--- src/main/java/it/unibo/jumpig/model/api/gameentity/AbstractPlatform.java
+++ src/main/java/it/unibo/jumpig/model/api/gameentity/AbstractPlatform.java
@@ -7,11 +7,20 @@ import it.unibo.jumpig.model.api.Velocity;
 import it.unibo.jumpig.model.api.collision.CollisionHandler;
 import it.unibo.jumpig.model.impl.VelocityImpl;
 
+/**
+ * Class to manage the behaviour of every platform in the world.
+ */
 public abstract class AbstractPlatform extends AbstractCollidableGameEntity<RectangleHitbox, Platform> implements Platform {
 
     private final Velocity jumpVelocity;
     private final double length = this.getHitbox().getWidth();
 
+    /**
+     * Constructor for any kind of platform.
+     * @param position position of the platform.
+     * @param verticalJumpVelocity vertical velocity of a player when jumps on the platform.
+     * @param collisionHandler the way is handled the collision with the player.
+     */
     public AbstractPlatform(final Position position, final double verticalJumpVelocity,
             final CollisionHandler<RectangleHitbox, Platform> collisionHandler) {
         super(position, new PlatformHitbox(position), collisionHandler);
