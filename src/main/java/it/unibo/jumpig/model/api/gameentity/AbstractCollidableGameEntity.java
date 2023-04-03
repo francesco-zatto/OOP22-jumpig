e6ac8e3c5014d4fe52427c89b051115a2f9211c9
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-04-03 19:04:37 +0200

    Implemented constructor and handleCollision in AbstractCollidableGameEntity

--- src/main/java/it/unibo/jumpig/model/api/gameentity/AbstractCollidableGameEntity.java
+++ src/main/java/it/unibo/jumpig/model/api/gameentity/AbstractCollidableGameEntity.java
@@ -2,18 +2,33 @@ package it.unibo.jumpig.model.api.gameentity;
 
 import it.unibo.jumpig.common.api.Position;
 import it.unibo.jumpig.common.api.hitbox.Hitbox;
+import it.unibo.jumpig.model.api.collision.CollisionHandler;
 
+/**
+ * Class to manage the way every collidable entity collides with the player.
+ * @param <H> any kind of Hitbox
+ */
 public abstract class AbstractCollidableGameEntity<H extends Hitbox> extends AbstractGameEntity<H> implements Collidable {
 
-    public AbstractCollidableGameEntity(Position position, H hitbox) {
+    private final CollisionHandler<H, GameEntity<H>> collisionHandler;
+
+    /**
+     * Constructor for any collidable gameEntity.
+     * @param position position of the gameEntity in the world.
+     * @param hitbox hitbox of the gameEntity.
+     * @param collisionHandler subtype of {@link it.unibo.jumpig.model.api.collision.CollisionHandler}
+     */
+    public AbstractCollidableGameEntity(final Position position, final H hitbox,
+            final CollisionHandler<H, GameEntity<H>> collisionHandler) {
         super(position, hitbox);
-        //TODO Auto-generated constructor stub
+        this.collisionHandler = collisionHandler;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void handleCollision(Player player) {
-        // TODO Auto-generated method stub
-        
+    public final void handleCollision(final Player player) {
+        this.collisionHandler.handle(player, this);
     }
-    
 }
