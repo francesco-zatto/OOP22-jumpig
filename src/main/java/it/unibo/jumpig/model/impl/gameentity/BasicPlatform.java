908bd1d9150b68ac7e79defa01d8f745d2b1461a
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-04-03 22:43:35 +0200

    Deleted methods and fields of BasicPlatform that are already in AbstractPlatform

--- src/main/java/it/unibo/jumpig/model/impl/gameentity/BasicPlatform.java
+++ src/main/java/it/unibo/jumpig/model/impl/gameentity/BasicPlatform.java
@@ -1,54 +1,20 @@
 package it.unibo.jumpig.model.impl.gameentity;
 
 import it.unibo.jumpig.common.api.Position;
-import it.unibo.jumpig.common.impl.hitbox.PlatformHitbox;
-import it.unibo.jumpig.common.impl.hitbox.RectangleHitbox;
-import it.unibo.jumpig.model.api.Velocity;
-import it.unibo.jumpig.model.api.gameentity.AbstractCollidableGameEntity;
-import it.unibo.jumpig.model.api.gameentity.Platform;
-import it.unibo.jumpig.model.api.gameentity.Player;
-import it.unibo.jumpig.model.impl.VelocityImpl;
+import it.unibo.jumpig.model.api.gameentity.AbstractPlatform;
 import it.unibo.jumpig.model.impl.collision.BasicPlatformCollisionHandler;
 
 /**
  * Class that represents a basic platform, that simply causes the player's jump.
  */
-public class BasicPlatform extends AbstractCollidableGameEntity<RectangleHitbox, Platform> implements Platform {
-
-    private final Velocity jumpVelocity;
-    private final double length = this.getHitbox().getWidth();
+public class BasicPlatform extends AbstractPlatform {
 
     /**
-     * Constructor for a basic platform.
-     * @param position position of the platform in the world.
-     * @param verticalJumpVelocity vertical velocity of a player when jumps on the platform. 
-    */
+     * Constructor for a basic platform that simply causes the jump of the player.
+     * @param position position of the platform.
+     * @param verticalJumpVelocity vertical velocity of a player when jumps on the platform.
+     */
     public BasicPlatform(final Position position, final double verticalJumpVelocity) {
-        super(position, new PlatformHitbox(position), new BasicPlatformCollisionHandler());
-        this.jumpVelocity = new VelocityImpl(0, verticalJumpVelocity);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Velocity getJumpVelocity() {
-        return this.jumpVelocity;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public double getLength() {
-        return this.length;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void handleCollision(final Player player) {
-        this.getCollisionHandler().handle(player, this);
+        super(position, verticalJumpVelocity, new BasicPlatformCollisionHandler());
     }
 }
