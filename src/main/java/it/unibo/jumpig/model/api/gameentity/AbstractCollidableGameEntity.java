f30cb98d1c13c2d86cfe8d341b37c6de0118fe6c
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-04-03 22:16:39 +0200

    Completed AbstractCollidableGameEntity with protected getCollisionHandler

--- src/main/java/it/unibo/jumpig/model/api/gameentity/AbstractCollidableGameEntity.java
+++ src/main/java/it/unibo/jumpig/model/api/gameentity/AbstractCollidableGameEntity.java
@@ -7,10 +7,12 @@ import it.unibo.jumpig.model.api.collision.CollisionHandler;
 /**
  * Class to manage the way every collidable entity collides with the player.
  * @param <H> any kind of Hitbox
+ * @param <E> any kind of gameEntity
  */
-public abstract class AbstractCollidableGameEntity<H extends Hitbox> extends AbstractGameEntity<H> implements Collidable {
+public abstract class AbstractCollidableGameEntity<H extends Hitbox, E extends GameEntity<H>> extends AbstractGameEntity<H>
+        implements Collidable {
 
-    private final CollisionHandler<H, GameEntity<H>> collisionHandler;
+    private final CollisionHandler<H, E> collisionHandler;
 
     /**
      * Constructor for any collidable gameEntity.
@@ -19,7 +21,7 @@ public abstract class AbstractCollidableGameEntity<H extends Hitbox> extends Abs
      * @param collisionHandler subtype of {@link it.unibo.jumpig.model.api.collision.CollisionHandler}
      */
     public AbstractCollidableGameEntity(final Position position, final H hitbox,
-            final CollisionHandler<H, GameEntity<H>> collisionHandler) {
+            final CollisionHandler<H, E> collisionHandler) {
         super(position, hitbox);
         this.collisionHandler = collisionHandler;
     }
@@ -28,7 +30,13 @@ public abstract class AbstractCollidableGameEntity<H extends Hitbox> extends Abs
      * {@inheritDoc}
      */
     @Override
-    public final void handleCollision(final Player player) {
-        this.collisionHandler.handle(player, this);
+    public abstract void handleCollision(Player player);
+
+    /**
+     * Getter for the collisionHandler of the entity of type E.
+     * @return the collision handler.
+     */
+    protected final CollisionHandler<H, E> getCollisionHandler() {
+        return this.collisionHandler;
     }
 }
