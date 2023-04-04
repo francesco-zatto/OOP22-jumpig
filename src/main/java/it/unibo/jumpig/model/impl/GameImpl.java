2cb33b9c4e93e6e811ccfa74f54f9355d8c3d18e
Francesco <francesco.filippini8@studio.unibo.it>
2023-04-04 18:43:06 +0200

    Added javadoc in unimplemented methods in GameImpl

--- src/main/java/it/unibo/jumpig/model/impl/GameImpl.java
+++ src/main/java/it/unibo/jumpig/model/impl/GameImpl.java
@@ -8,18 +8,27 @@ import it.unibo.jumpig.model.api.Game;
 
 public class GameImpl implements Game {
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean isOVer() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'isOVer'");
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void updateGame() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'updateGame'");
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getCurrentScore() {
         // TODO Auto-generated method stub
