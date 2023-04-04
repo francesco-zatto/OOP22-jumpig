4337059e64c216c09c9a3b2aa8d166ece9917028
Francesco <francesco.filippini8@studio.unibo.it>
2023-04-04 15:01:40 +0200

    Added javadoc in methods in GameControllerImpl

--- src/main/java/it/unibo/jumpig/controller/impl/GameControllerImpl.java
+++ src/main/java/it/unibo/jumpig/controller/impl/GameControllerImpl.java
@@ -8,30 +8,45 @@ import it.unibo.jumpig.controller.api.GameController;
 
 public class GameControllerImpl implements GameController {
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void mainLoop() {
         // TODO Auto-generated method stub
         
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void notifyUpdate() {
         // TODO Auto-generated method stub
         
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void registerInput() {
         // TODO Auto-generated method stub
         
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void close() {
         // TODO Auto-generated method stub
         
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void start() {
         // TODO Auto-generated method stub
