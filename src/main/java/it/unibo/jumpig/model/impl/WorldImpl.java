a109cf69bfc4c76b1cbe279e54baea5cb0c3addf
Francesco <francesco.filippini8@studio.unibo.it>
2023-04-04 18:47:40 +0200

    Created class WorldImpl

---
+++ src/main/java/it/unibo/jumpig/model/impl/WorldImpl.java
@@ -0,0 +1,11 @@
+package it.unibo.jumpig.model.impl;
+
+import it.unibo.jumpig.model.api.World;
+
+/**
+ * The class to manage the world of the game.
+ */
+
+public class WorldImpl implements World {
+    
+}
