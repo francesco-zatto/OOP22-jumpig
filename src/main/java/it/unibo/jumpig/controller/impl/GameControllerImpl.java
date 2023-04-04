2bfcaa8c72cfb2cc6646c593dd6618230d4ad518
Francesco <francesco.filippini8@studio.unibo.it>
2023-04-04 14:57:48 +0200

    Created class GameControllerImpl

---
+++ src/main/java/it/unibo/jumpig/controller/impl/GameControllerImpl.java
@@ -0,0 +1,11 @@
+package it.unibo.jumpig.controller.impl;
+
+import it.unibo.jumpig.controller.api.GameController;
+
+/**
+ *The class to manage the game loop.
+ */
+
+public class GameControllerImpl implements GameController {
+    
+}
