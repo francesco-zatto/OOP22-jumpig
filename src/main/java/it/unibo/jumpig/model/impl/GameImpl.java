7ceba69ffceec7c93078d23d258584de0f432b4c
Francesco <francesco.filippini8@studio.unibo.it>
2023-04-04 18:39:18 +0200

    Added unimplemented methods in GameImpl

---
+++ src/main/java/it/unibo/jumpig/model/impl/GameImpl.java
@@ -0,0 +1,29 @@
+package it.unibo.jumpig.model.impl;
+
+import it.unibo.jumpig.model.api.Game;
+
+/**
+ * The class to manage a game.
+ */
+
+public class GameImpl implements Game {
+
+    @Override
+    public boolean isOVer() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isOVer'");
+    }
+
+    @Override
+    public void updateGame() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateGame'");
+    }
+
+    @Override
+    public int getCurrentScore() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCurrentScore'");
+    }
+    
+}
