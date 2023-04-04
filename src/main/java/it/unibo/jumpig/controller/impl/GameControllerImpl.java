0d37ade81b4a4e785c04d9dc2865f409922a7850
Francesco <francesco.filippini8@studio.unibo.it>
2023-04-04 14:58:52 +0200

    Added unimplemented methods in GameControllerImpl

--- src/main/java/it/unibo/jumpig/controller/impl/GameControllerImpl.java
+++ src/main/java/it/unibo/jumpig/controller/impl/GameControllerImpl.java
@@ -7,5 +7,35 @@ import it.unibo.jumpig.controller.api.GameController;
  */
 
 public class GameControllerImpl implements GameController {
+
+    @Override
+    public void mainLoop() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void notifyUpdate() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void registerInput() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void close() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void start() {
+        // TODO Auto-generated method stub
+        
+    }
     
 }
