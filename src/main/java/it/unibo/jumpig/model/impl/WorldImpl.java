5063abbab34fac4b77fdb18703c378b74bc0a0c0
Francesco <francesco.filippini8@studio.unibo.it>
2023-04-04 18:49:51 +0200

    Added javadoc in unimplemented methods in WorldImpl

--- src/main/java/it/unibo/jumpig/model/impl/WorldImpl.java
+++ src/main/java/it/unibo/jumpig/model/impl/WorldImpl.java
@@ -14,30 +14,45 @@ import it.unibo.jumpig.model.api.gameentity.Player;
 
 public class WorldImpl implements World {
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public double getGravity() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getGravity'");
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Set<Platform> getPlatform() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getPlatform'");
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Player getPlayer() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getPlayer'");
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Set<Enemy> getEnemies() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getEnemies'");
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Set<Coin> getCoins() {
         // TODO Auto-generated method stub
