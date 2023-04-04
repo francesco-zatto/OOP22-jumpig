f69ef1fc2b3044e669bdf0bc4fdd0f858201fa30
Francesco <francesco.filippini8@studio.unibo.it>
2023-04-04 18:48:26 +0200

    Added unimplemented methods in WorldImpl

--- src/main/java/it/unibo/jumpig/model/impl/WorldImpl.java
+++ src/main/java/it/unibo/jumpig/model/impl/WorldImpl.java
@@ -1,11 +1,47 @@
 package it.unibo.jumpig.model.impl;
 
+import java.util.Set;
+
 import it.unibo.jumpig.model.api.World;
+import it.unibo.jumpig.model.api.gameentity.Coin;
+import it.unibo.jumpig.model.api.gameentity.Enemy;
+import it.unibo.jumpig.model.api.gameentity.Platform;
+import it.unibo.jumpig.model.api.gameentity.Player;
 
 /**
  * The class to manage the world of the game.
  */
 
 public class WorldImpl implements World {
+
+    @Override
+    public double getGravity() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getGravity'");
+    }
+
+    @Override
+    public Set<Platform> getPlatform() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPlatform'");
+    }
+
+    @Override
+    public Player getPlayer() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPlayer'");
+    }
+
+    @Override
+    public Set<Enemy> getEnemies() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getEnemies'");
+    }
+
+    @Override
+    public Set<Coin> getCoins() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCoins'");
+    }
     
 }
