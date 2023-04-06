d0ee378c3ec491b4cb0b87cd6347c8276fdea11d
Francesco <francesco.filippini8@studio.unibo.it>
2023-04-06 16:56:55 +0200

    Added constructor and its javadoc in CoinHitbox

--- src/main/java/it/unibo/jumpig/common/impl/hitbox/CoinHitbox.java
+++ src/main/java/it/unibo/jumpig/common/impl/hitbox/CoinHitbox.java
@@ -1,9 +1,20 @@
 package it.unibo.jumpig.common.impl.hitbox;
 
+import it.unibo.jumpig.common.api.Position;
+
 /**
  * The class that represents a coin's hitbox.
  */
 
 public class CoinHitbox extends CircleHitbox {
-    
+
+    private static final double COIN_RADIUS = 1; //TODO
+
+    /**
+     * Constructor for the coin's hitbox.
+     * @param center the center of the hitbox.
+     */
+    public CoinHitbox(final Position center) {
+        super(center, COIN_RADIUS);
+    }
 }
