14b55f53729cef63afbd97301562f3691a2a883a
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-04-11 16:29:17 +0200

    Added constructor and paint method in GamePanel

--- src/main/java/it/unibo/jumpig/view/impl/GamePanel.java
+++ src/main/java/it/unibo/jumpig/view/impl/GamePanel.java
@@ -1,5 +1,8 @@
 package it.unibo.jumpig.view.impl;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
 /**
@@ -8,4 +11,20 @@ import javax.swing.JPanel;
 public class GamePanel extends JPanel { 
 
     public static final long serialVersionUID = 1L;
+
+    /**
+     * Constructor for a gamePanel.
+     */
+    public GamePanel() {
+        this.setBackground(Color.CYAN);
+    }
+
+    /**
+     * Method that paints every drawing of the gamePanel.
+     * @param g graphics used to paint.
+     */
+    @Override
+    public void paint(final Graphics g) {
+        super.paintAll(g);
+    }
 }
