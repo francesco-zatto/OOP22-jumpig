81fb247f15384e7e516fdc5404e966057e725bc7
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-04-11 16:17:25 +0200

    Created GamePanel

---
+++ src/main/java/it/unibo/jumpig/view/impl/GamePanel.java
@@ -0,0 +1,11 @@
+package it.unibo.jumpig.view.impl;
+
+import javax.swing.JPanel;
+
+/**
+ * The GUI that shows the game currently going on.
+ */
+public class GamePanel extends JPanel { 
+
+    public static final long serialVersionUID = 1L;
+}
