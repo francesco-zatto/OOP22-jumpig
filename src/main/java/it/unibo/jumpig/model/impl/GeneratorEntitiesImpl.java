6917898039d498aa1f46b90ba3e49fcd65fbacf2
Francesco <francesco.filippini8@studio.unibo.it>
2023-04-12 13:40:10 +0200

    Added unimplemented methods in GeneratorEntitiesImpl

--- src/main/java/it/unibo/jumpig/model/impl/GeneratorEntitiesImpl.java
+++ src/main/java/it/unibo/jumpig/model/impl/GeneratorEntitiesImpl.java
@@ -1,11 +1,34 @@
 package it.unibo.jumpig.model.impl;
 
+import java.util.Set;
+
 import it.unibo.jumpig.model.api.GeneratorEntities;
+import it.unibo.jumpig.model.api.gameentity.Coin;
+import it.unibo.jumpig.model.api.gameentity.Enemy;
+import it.unibo.jumpig.model.api.gameentity.Platform;
 
 /**
  * The class to manage the generation of the entities.
  */
 
 public class GeneratorEntitiesImpl implements GeneratorEntities {
+
+    @Override
+    public Set<Platform> generatePlatforms() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'generatePlatforms'");
+    }
+
+    @Override
+    public Set<Enemy> generateEnemies() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'generateEnemies'");
+    }
+
+    @Override
+    public Set<Coin> generateCoins() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'generateCoins'");
+    }
     
 }
