ed40cbbfb0d31cd0437891d407f9dd99723e81e3
Francesco <francesco.filippini8@studio.unibo.it>
2023-04-12 12:11:27 +0200

    Added unimplemented method getHeight() in CameraImpl

---
+++ src/main/java/it/unibo/jumpig/model/impl/CameraImpl.java
@@ -0,0 +1,20 @@
+package it.unibo.jumpig.model.impl;
+
+import it.unibo.jumpig.model.api.Camera;
+
+/**
+ * The class to manage the camera.
+ */
+
+public class CameraImpl implements Camera {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getHeight() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getHeight'");
+    }
+
+}
