457529dbd5fe68c6eba8daed58b8b345a746fb19
Francesco <francesco.filippini8@studio.unibo.it>
2023-04-12 13:39:31 +0200

    Created class GeneratorEntitiesImpl

---
+++ src/main/java/it/unibo/jumpig/model/impl/GeneratorEntitiesImpl.java
@@ -0,0 +1,11 @@
+package it.unibo.jumpig.model.impl;
+
+import it.unibo.jumpig.model.api.GeneratorEntities;
+
+/**
+ * The class to manage the generation of the entities.
+ */
+
+public class GeneratorEntitiesImpl implements GeneratorEntities {
+    
+}
