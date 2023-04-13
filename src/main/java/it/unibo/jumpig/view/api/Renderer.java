d188b37c1ea39549ee74f9269a7228eedfbcc47c
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-04-13 17:01:25 +0200

    Added render methods for every type of gameEntity in Renderer

--- src/main/java/it/unibo/jumpig/view/api/Renderer.java
+++ src/main/java/it/unibo/jumpig/view/api/Renderer.java
@@ -1,5 +1,6 @@
 package it.unibo.jumpig.view.api;
 
+import it.unibo.jumpig.common.impl.hitbox.CircleHitbox;
 import it.unibo.jumpig.common.impl.hitbox.RectangleHitbox;
 import it.unibo.jumpig.model.api.gameentity.GameEntity;
 
@@ -13,4 +14,28 @@ public interface Renderer {
      * @param entity player to render.
      */
     void renderPlayer(GameEntity<RectangleHitbox> entity);
+
+    /**
+     * Method that renders an enemy in the View.
+     * @param entity enemy to render.
+     */
+    void renderEnemy(GameEntity<RectangleHitbox> entity);
+
+    /**
+     * Method that renders a basic platform in the View.
+     * @param entity basic platform to render.
+     */
+    void renderBasicPlatform(GameEntity<RectangleHitbox> entity);
+
+    /**
+     * Method that renders a vanishing platform in the View.
+     * @param entity vanishing platform to render.
+     */
+    void renderVanishingPlatform(GameEntity<RectangleHitbox> entity);
+
+    /**
+     * Method that renders a coin in the View.
+     * @param entity coin to render.
+     */
+    void renderCoin(GameEntity<CircleHitbox> entity);
 }
