bb28eb7247675be4810376e240249b8cec93c318
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-04-14 21:36:22 +0200

    Modified VanishingPlatform because of AbstractPlatform class

--- src/main/java/it/unibo/jumpig/model/impl/gameentity/VanishingPlatform.java
+++ src/main/java/it/unibo/jumpig/model/impl/gameentity/VanishingPlatform.java
@@ -1,6 +1,7 @@
 package it.unibo.jumpig.model.impl.gameentity;
 
 import it.unibo.jumpig.common.api.Position;
+import it.unibo.jumpig.model.api.gameentity.AbstractPlatform;
 import it.unibo.jumpig.model.api.gameentity.Player;
 import it.unibo.jumpig.model.api.gameentity.Targettable;
 import it.unibo.jumpig.model.impl.collision.VanishingPlatformCollisionHandler;
@@ -8,18 +9,18 @@ import it.unibo.jumpig.model.impl.collision.VanishingPlatformCollisionHandler;
 /**
  * Class that represents a platform which, when the player jumps on it, it vanishes.
  */
-public class VanishingPlatform extends BasicPlatform implements Targettable {
+public class VanishingPlatform extends AbstractPlatform implements Targettable {
 
     private boolean targettable;
-    private final VanishingPlatformCollisionHandler collisionHandler;
+    private final VanishingPlatformCollisionHandler collisionHandler = new VanishingPlatformCollisionHandler();
+
     /**
      * Constructor for a vanishing platform.
      * @param position position of the platform in the world.
      * @param verticalJumpVelocity vertical velocity of a player when jumps on the platform. 
     */
     public VanishingPlatform(final Position position, final double verticalJumpVelocity) {
-        super(position, verticalJumpVelocity);
-        this.collisionHandler = new VanishingPlatformCollisionHandler();
+        super(position, verticalJumpVelocity, null);
     }
 
     /**
@@ -38,11 +39,8 @@ public class VanishingPlatform extends BasicPlatform implements Targettable {
         return this.targettable;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
-    public void handleCollision(final Player player) {
+    public final void handleCollision(final Player player) {
         this.collisionHandler.handle(player, this);
     }
 }
