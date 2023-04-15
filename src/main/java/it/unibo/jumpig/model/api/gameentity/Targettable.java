a298251cbfc4337a80036d9558662ccb712a9911
2Lgigi <luigi.li@studio.unibo.it>
2023-04-15 19:45:47 +0200

    Renamed method setTaken() in markTarget()

--- src/main/java/it/unibo/jumpig/model/api/gameentity/Targettable.java
+++ src/main/java/it/unibo/jumpig/model/api/gameentity/Targettable.java
@@ -1,18 +1,20 @@
 package it.unibo.jumpig.model.api.gameentity;
+
 /**
- * The interface to understand if a targettable entity has to remain visible or not.
+ * The interface to understand if a targettable entity has to remain visible or
+ * not.
  * For example, if a coin has been taken it has to disappear.
  */
 public interface Targettable {
 
     /**
-     * The method to set the targettability of a targettable entity. 
-     * @param setTargettable is true if the entity has to be set as taken,i.e. it will disappear.
+     * When this method is called the entity will disappear.
      */
-    void setTarget(boolean setTargettable);
+    void markTarget();
 
     /**
      * The method to know if the targettable entity has to disappear.
+     * 
      * @return true if the entity has to disappear.
      */
     boolean isTaken();
