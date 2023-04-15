3e4daea6aafbf15b0766ec63dc97fa9f9f28c852
2Lgigi <luigi.li@studio.unibo.it>
2023-04-15 19:41:43 +0200

    Fixed an error

--- src/main/java/it/unibo/jumpig/model/api/gameentity/Coin.java
+++ src/main/java/it/unibo/jumpig/model/api/gameentity/Coin.java
@@ -5,5 +5,6 @@ import it.unibo.jumpig.common.impl.hitbox.CircleHitbox;
 /**
  * An interface that represents the coin which is collectible by the player.
  */
-public interface Coin extends GameEntity<CircleHitbox>, Targettable {
+public interface Coin extends GameEntity<CircleHitbox>, Targettable, Collidable {
+
 }
