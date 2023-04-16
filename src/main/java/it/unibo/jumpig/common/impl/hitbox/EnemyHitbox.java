377f3cde965c35c43bc67345c8dec9f6cb33b043
Francesco <francesco.filippini8@studio.unibo.it>
2023-04-16 21:03:23 +0200

    Implemented class EnemyHitbox

--- src/main/java/it/unibo/jumpig/common/impl/hitbox/EnemyHitbox.java
+++ src/main/java/it/unibo/jumpig/common/impl/hitbox/EnemyHitbox.java
@@ -1,9 +1,21 @@
 package it.unibo.jumpig.common.impl.hitbox;
 
+import it.unibo.jumpig.common.api.Position;
+
 /**
  * The class that represent an enemy's hitbox.
  */
 
 public class EnemyHitbox extends RectangleHitbox {
-    
+
+    private static final double ENEMY_WIDTH = 8;
+    private static final double ENEMY_HEIGHT = 8;
+
+    /**
+     * Constructor for the enemy's hitbox.
+     * @param center the center of the hitbox.
+     */
+    public EnemyHitbox(final Position center) {
+        super(center, ENEMY_WIDTH, ENEMY_HEIGHT);
+    }
 }
