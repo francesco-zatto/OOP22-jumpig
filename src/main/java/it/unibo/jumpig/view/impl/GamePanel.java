e504e2e27672dc4f150df8583c2b7dd554d0f66e
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-04-16 13:38:42 +0200

    Deleted every reference to Platform in GamePanel

--- src/main/java/it/unibo/jumpig/view/impl/GamePanel.java
+++ src/main/java/it/unibo/jumpig/view/impl/GamePanel.java
@@ -4,20 +4,13 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Point;
-import java.awt.Rectangle;
 import java.awt.Toolkit;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 //import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 //import it.unibo.jumpig.common.impl.PositionImpl;
-import it.unibo.jumpig.common.impl.hitbox.RectangleHitbox;
-import it.unibo.jumpig.model.api.gameentity.Platform;
 //import it.unibo.jumpig.model.impl.gameentity.BasicPlatform;
 
 /**
@@ -26,30 +19,20 @@ import it.unibo.jumpig.model.api.gameentity.Platform;
 public class GamePanel extends JPanel { 
 
     public static final long serialVersionUID = 1L;
-    private static final double WORLD_WIDTH = 36; //tmp costant
-    private static final double WORLD_HEIGHT = 64; //tmp costant
+    private final double worldWidth;
+    private final double worldHeight;
     private final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
     private final Dimension startScreen = new Dimension((int) screen.getWidth() / 5, (int) (screen.getWidth() / 5 * 1.7));
-    //private final double WORLD_WIDTH = 1;
-    //private final double WORLD_HEIGHT = 0.5;
-    private final Set<Rectangle> platforms = new HashSet<>();
 
     /**
      * Constructor for a gamePanel.
      * @param platformSet a set of platform for testing
      */
-    public GamePanel(final Set<Platform> platformSet) { //TODO SBAGLIATISSIMO
+    public GamePanel(final double worldWidth, final double worldHeight) { 
+        this.worldWidth = worldWidth;
+        this.worldHeight = worldHeight;
         this.setSize(this.startScreen);
-        this.setPreferredSize(super.getSize());
-        this.platforms.addAll(platformSet.stream()
-                .map(Platform::getHitbox)
-                .map(this::convertHitboxToRectangle)
-                .collect(Collectors.toSet()));
-    }
-
-    private Rectangle convertHitboxToRectangle(final RectangleHitbox hitbox) {
-        return new Rectangle(new Point((int) hitbox.getCenter().getX(), (int) hitbox.getCenter().getY()),
-                new Dimension((int) (hitbox.getWidth()), (int) (hitbox.getHeight())));
+        this.setPreferredSize(this.getSize());
     }
 
     /**
@@ -63,17 +46,6 @@ public class GamePanel extends JPanel {
         final Graphics2D g2D = (Graphics2D) g;
         g2D.setBackground(Color.CYAN);
         super.paint(g2D);
-        g2D.setColor(Color.RED);
-        this.platforms.stream()
-                .map(this::scale).forEach(g2D::fill);
-    }
-
-    private Rectangle scale(final Rectangle rectangle) {
-        return new Rectangle(new Point((int) (rectangle.getCenterX() * this.getWidth() / WORLD_WIDTH),
-                (int) (rectangle.getCenterY() * this.getHeight() / WORLD_HEIGHT)),
-                new Dimension((int) (rectangle.getWidth() * this.getWidth() / WORLD_WIDTH),
-                        (int) (rectangle.getHeight() * this.getHeight() / WORLD_HEIGHT)));
-
     }
 
     /*public static void main(String[] args) {
