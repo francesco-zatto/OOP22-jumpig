70cf7a2faaf0fe4117a2a760694acd15c2c01637
Francesco <francesco.filippini8@studio.unibo.it>
2023-04-17 23:57:25 +0200

    Added unimplemented methods in GameViewImpl

--- src/main/java/it/unibo/jumpig/view/impl/GameViewImpl.java
+++ src/main/java/it/unibo/jumpig/view/impl/GameViewImpl.java
@@ -1,5 +1,8 @@
 package it.unibo.jumpig.view.impl;
 
+import java.util.Set;
+
+import it.unibo.jumpig.common.api.hitbox.Hitbox;
 import it.unibo.jumpig.view.api.GameViewScene;
 
 /**
@@ -7,5 +10,29 @@ import it.unibo.jumpig.view.api.GameViewScene;
  */
 
 public class GameViewImpl implements GameViewScene {
-    
+
+    @Override
+    public void show() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'show'");
+    }
+
+    @Override
+    public void quit() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'quit'");
+    }
+
+    @Override
+    public void renderEntities(final Set<Hitbox> entities) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'renderEntities'");
+    }
+
+    @Override
+    public void renderCurrentScore(final int coins, final int height, final int lives) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'renderCurrentScore'");
+    }
+
 }
