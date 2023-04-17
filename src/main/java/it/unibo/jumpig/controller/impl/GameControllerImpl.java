4240431fdfbb57074aa4cbcd20b535b63d9611ff
Alessandro <alessandro.verna@studio.unibo.it>
2023-04-17 14:15:10 +0200

    Created class GameControllerImpl

---
+++ src/main/java/it/unibo/jumpig/controller/impl/GameControllerImpl.java
@@ -0,0 +1,45 @@
+package it.unibo.jumpig.controller.impl;
+
+import it.unibo.jumpig.controller.api.GameController;
+
+/**
+ * Class that controls the game.
+ */
+public class GameControllerImpl implements GameController {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void start() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void close() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'close'");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void registerInput() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'registerInput'");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void notifyUpdate() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'notifyUpdate'");
+    }
+}
