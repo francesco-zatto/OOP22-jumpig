a71903d01f30115cb09bc96140bb0c1872e56e0c
Francesco <francesco.filippini8@studio.unibo.it>
2023-04-17 23:54:23 +0200

    Created class GameViewImpl

---
+++ src/main/java/it/unibo/jumpig/view/impl/GameViewImpl.java
@@ -0,0 +1,11 @@
+package it.unibo.jumpig.view.impl;
+
+import it.unibo.jumpig.view.api.GameViewScene;
+
+/**
+ * The class to manage updates of score and updates of the game.
+ */
+
+public class GameViewImpl implements GameViewScene {
+    
+}
