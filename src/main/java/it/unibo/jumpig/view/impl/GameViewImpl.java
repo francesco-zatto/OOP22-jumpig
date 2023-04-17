d51d2f674749c4c0947b2cd20f233d263c945fe5
Francesco <francesco.filippini8@studio.unibo.it>
2023-04-17 23:59:08 +0200

    Added javadoc for unimplemented methods in GameViewImpl

--- src/main/java/it/unibo/jumpig/view/impl/GameViewImpl.java
+++ src/main/java/it/unibo/jumpig/view/impl/GameViewImpl.java
@@ -11,24 +11,36 @@ import it.unibo.jumpig.view.api.GameViewScene;
 
 public class GameViewImpl implements GameViewScene {
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void show() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'show'");
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void quit() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'quit'");
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void renderEntities(final Set<Hitbox> entities) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'renderEntities'");
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void renderCurrentScore(final int coins, final int height, final int lives) {
         // TODO Auto-generated method stub
