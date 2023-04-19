22c34d8399c988ea47d76e6aba9f766333f20ea3
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-04-19 14:17:55 +0200

    Added abstract methods in AbstractCollisionChecker

--- src/main/java/it/unibo/jumpig/model/api/collision/AbstractCollisionChecker.java
+++ src/main/java/it/unibo/jumpig/model/api/collision/AbstractCollisionChecker.java
@@ -9,14 +9,37 @@ import it.unibo.jumpig.model.api.gameentity.Player;
  * @param <H> any kind of hitbox
  * @param <E> any kind of game entity
  */
-public class AbstractCollisionChecker<H extends Hitbox, E extends GameEntity<H>> implements CollisionChecker<H, E> {
+public abstract class AbstractCollisionChecker<H extends Hitbox, E extends GameEntity<H>> implements CollisionChecker<H, E> {
 
     /**
      * {@inheritDoc}
      */
     @Override
     public boolean check(final Player player, final E gameEntity) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'check'");
+        return this.areBoundsColliding(player, gameEntity)
+            && this.canPlayerCollide(player)
+            && this.canEntityCollide(gameEntity);
     }
+
+    /**
+     * Method that checks if the hitboxes are colliding.
+     * @param player player to check if it is colliding.
+     * @param gameEntity gameEntity to check if it is coliding.
+     * @return true if player's and gameEntity's bounds are colliding.
+     */
+    protected abstract boolean areBoundsColliding(Player player, E gameEntity);
+
+    /**
+     * Method that checks if the player in this moment can collide or not.
+     * @param player player to check.
+     * @return true if the player can collide.
+     */
+    protected abstract boolean canPlayerCollide(Player player);
+
+    /**
+     * Method that checks if the player in this moment can collide or not.
+     * @param gameEntity gameEntity to check.
+     * @return true if the gameEntity can collide.
+     */
+    protected abstract boolean canEntityCollide(E gameEntity);
 }
