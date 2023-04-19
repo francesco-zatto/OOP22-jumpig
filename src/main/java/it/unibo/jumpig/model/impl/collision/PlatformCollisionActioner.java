858509e25bdb12a24911d9f7b5b2861d904270d9
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-04-19 19:31:32 +0200

    Created PlatformCollisionActioner

---
+++ src/main/java/it/unibo/jumpig/model/impl/collision/PlatformCollisionActioner.java
@@ -0,0 +1,32 @@
+package it.unibo.jumpig.model.impl.collision;
+
+import it.unibo.jumpig.common.impl.hitbox.RectangleHitbox;
+import it.unibo.jumpig.model.api.collision.AbstractCollisionActioner;
+import it.unibo.jumpig.model.api.gameentity.Platform;
+import it.unibo.jumpig.model.api.gameentity.Player;
+import it.unibo.jumpig.model.impl.VelocityImpl;
+
+/**
+ * Class that changes player's velocity after a jump on a platform.
+ * @param <P> any kind of platform.
+ */
+public class PlatformCollisionActioner<P extends Platform> extends AbstractCollisionActioner<RectangleHitbox, P> {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void actOnPlayer(final Player player, final P gameEntity) {
+        var platformJumpVelocity = gameEntity.getJumpVelocity();
+        player.setVelocityFromJump(
+            new VelocityImpl(platformJumpVelocity.getXComponent(), platformJumpVelocity.getYComponent())
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void actOnEntity(final Player player, final P gameEntity) {
+    }
+}
