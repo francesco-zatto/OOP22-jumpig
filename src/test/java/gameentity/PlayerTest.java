7dd800c852447b6e0b7c5d4cea9bd4592cd06f38
Alessandro <alessandro.verna@studio.unibo.it>
2023-04-28 12:40:58 +0200

    Created test testPlayervelocity

--- src/test/java/gameentity/PlayerTest.java
+++ src/test/java/gameentity/PlayerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import it.unibo.jumpig.common.api.Position;
 import it.unibo.jumpig.common.impl.PositionImpl;
+import it.unibo.jumpig.model.api.Velocity;
 import it.unibo.jumpig.model.api.gameentity.Player;
 import it.unibo.jumpig.model.impl.VelocityImpl;
 import it.unibo.jumpig.model.impl.gameentity.PlayerImpl;
@@ -18,14 +19,29 @@ class PlayerTest {
 
     private static final double PLAYER_COMPONENT_X = 5;
     private static final double PLAYER_COMPONENT_Y = 5;
+    final Velocity finalVelocity = new VelocityImpl(0,10);
+    private static final double gravity = 10;
+    private static final double deltaTime = 2;
     private final Player player = new PlayerImpl(new PositionImpl(0, 0));
 
     @Test
     void testPlayerPosition() {
         final Position finalPosition = new PositionImpl(PLAYER_COMPONENT_X, PLAYER_COMPONENT_Y);
         player.setVelocityFromJump(new VelocityImpl(PLAYER_COMPONENT_X, PLAYER_COMPONENT_Y));
-        player.computePosition(2);
+        player.computePosition(deltaTime);
         assertEquals(player.getPosition().getX(), finalPosition.getX() * 2);
         assertEquals(player.getPosition().getY(), finalPosition.getY() * 2);
+    }
+
+    @Test 
+    void testPlayerVelocity() {
+        final Velocity playerVelocity = finalVelocity;
+        player.setVelocityFromJump(playerVelocity);
+        System.out.println(player.getVelocity().getYComponent());
+        System.out.println(finalVelocity.getYComponent());
+        player.computeVelocity(gravity, deltaTime);
+        System.out.println(finalVelocity.getYComponent());       
+        System.out.println(player.getVelocity().getYComponent());
+        assertEquals(finalVelocity.getModule(), player.getVelocity().getYComponent());
     }
 }
