32e35262d6f8794e06183a69697020724c17c0aa
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-04-28 17:38:16 +0200

    Added tests in CollisionHandlerTest

--- src/test/java/collision/CollisionHandlerTest.java
+++ src/test/java/collision/CollisionHandlerTest.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 import it.unibo.jumpig.common.api.Position;
 import it.unibo.jumpig.common.impl.PositionImpl;
+import it.unibo.jumpig.model.api.gameentity.Coin;
 import it.unibo.jumpig.model.api.gameentity.Targettable;
 import it.unibo.jumpig.model.impl.gameentity.BasicCoin;
 import it.unibo.jumpig.model.impl.gameentity.EnemyImpl;
@@ -23,6 +27,7 @@ class CollisionHandlerTest {
     private static final double COIN_POSITION_Y = 7.5;
     private static final double ENEMY_POSITION_X = 6;
     private static final double ENEMY_POSITION_Y = 6.5;
+    private static final int NUMBER_OF_COINS = 10;
     private static final Position PLAYER_POSITION = new PositionImpl(PLAYER_POSITION_X, PLAYER_POSITION_Y);
     private static final Position COIN_POSITION = new PositionImpl(COIN_POSITION_X, COIN_POSITION_Y);
     private static final Position ENEMY_POSITION = new PositionImpl(ENEMY_POSITION_X, ENEMY_POSITION_Y);
@@ -39,6 +44,30 @@ class CollisionHandlerTest {
         coin.handleCollision(player);
         assertEquals(pickedCoins + 1, player.getCoins());
         assertIsTaken(coin);
+    }
+
+    @Test
+    void testManyCoinCollisions() {
+        final var player = new PlayerImpl(PLAYER_POSITION);
+        final List<? extends Coin> coins = Stream.iterate(0, i -> i + 1)
+                .limit(NUMBER_OF_COINS)
+                .map(i -> COIN_POSITION)
+                .map(BasicCoin::new)
+                .toList();
+        final double pickedCoins = player.getCoins();
+        coins.forEach(c -> c.handleCollision(player));
+        assertEquals(pickedCoins + NUMBER_OF_COINS, player.getCoins());
+        coins.forEach(CollisionHandlerTest::assertIsTaken);
+    }
+
+    @Test
+    void testCoinIsAlreadyPicked() {
+        final var player = new PlayerImpl(PLAYER_POSITION);
+        final var coin = new BasicCoin(COIN_POSITION);
+        coin.handleCollision(player);
+        final double pickedCoins = player.getCoins();
+        coin.handleCollision(player);
+        assertEquals(pickedCoins, player.getCoins());
     }
 
     @Test
