22923a24bf67f3035051d5b9e468571e85ff6cdb
Alessandro <alessandro.verna@studio.unibo.it>
2023-05-02 22:08:43 +0200

    Created constructor and field controller

--- src/main/java/it/unibo/jumpig/view/impl/PlayerKeyListener.java
+++ src/main/java/it/unibo/jumpig/view/impl/PlayerKeyListener.java
@@ -3,27 +3,49 @@ package it.unibo.jumpig.view.impl;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import it.unibo.jumpig.controller.api.GameController;
+
+/**
+ * Class that manages the input from the keyboard.
+ */
 public class PlayerKeyListener implements KeyListener {
 
+    private final GameController controller;
+    /**
+     * Constructor for the PlayerKeyListener.
+     * @param controller the controller 
+     */
+    public PlayerKeyListener(final GameController controller) {
+        this.controller = controller;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void keyTyped(KeyEvent e) {
+    public void keyTyped(final KeyEvent e) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void keyPressed(KeyEvent e) {
-        if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+    public void keyPressed(final KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
             System.out.println("Player si muove a sinistra");
-        } else if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
             System.out.println("Player si muove a destra");
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void keyReleased(KeyEvent e) {
+    public void keyReleased(final KeyEvent e) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
     }
-    
 }
