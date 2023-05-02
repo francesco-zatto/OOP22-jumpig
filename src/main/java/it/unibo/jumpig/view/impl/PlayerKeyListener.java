0c47c3449df8813cda59bfd2ddb15e12dc9e3acf
Alessandro <alessandro.verna@studio.unibo.it>
2023-05-02 21:27:17 +0200

    Created class PlayerKeyListener

---
+++ src/main/java/it/unibo/jumpig/view/impl/PlayerKeyListener.java
@@ -0,0 +1,26 @@
+package it.unibo.jumpig.view.impl;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class PlayerKeyListener implements KeyListener {
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
+    }
+    
+}
