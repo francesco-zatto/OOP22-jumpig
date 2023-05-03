e116720a6291d7baa8ccd53578aeadfc6d238015
Alessandro <alessandro.verna@studio.unibo.it>
2023-05-03 21:41:22 +0200

    Deleted parameter direction

--- src/main/java/it/unibo/jumpig/controller/api/GameController.java
+++ src/main/java/it/unibo/jumpig/controller/api/GameController.java
@@ -6,9 +6,8 @@ package it.unibo.jumpig.controller.api;
 public interface GameController extends Status {
     /**
      * This method register the input from the keyboard while in game.
-     * @param direction the direction the player moves (-1 sx, 1 dx, otherwise 0)
      */
-    void registerInput(int direction);
+    void registerInput();
 
     /**
      * This method notify any update from the game.
