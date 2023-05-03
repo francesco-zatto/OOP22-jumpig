4ce277a1fa3cfaecef389d5ddfd31570e468e2bd
Alessandro <alessandro.verna@studio.unibo.it>
2023-05-03 21:35:07 +0200

    Added parameter direction

--- src/main/java/it/unibo/jumpig/controller/api/GameController.java
+++ src/main/java/it/unibo/jumpig/controller/api/GameController.java
@@ -6,8 +6,9 @@ package it.unibo.jumpig.controller.api;
 public interface GameController extends Status {
     /**
      * This method register the input from the keyboard while in game.
+     * @param direction the direction the player moves (-1 sx, 1 dx, otherwise 0)
      */
-    void registerInput();
+    void registerInput(int direction);
 
     /**
      * This method notify any update from the game.
