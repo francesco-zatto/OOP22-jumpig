92a390fa29a398c98497e7716ace3bc8e848a497
Alessandro <alessandro.verna@studio.unibo.it>
2023-05-03 21:49:52 +0200

    Fixed javadoc

--- src/main/java/it/unibo/jumpig/controller/api/GameController.java
+++ src/main/java/it/unibo/jumpig/controller/api/GameController.java
@@ -6,6 +6,7 @@ package it.unibo.jumpig.controller.api;
 public interface GameController extends Status {
     /**
      * This method register the input from the keyboard while in game.
+     * @param input the keyboard's input.
      */
     void registerInput(int input);
 
