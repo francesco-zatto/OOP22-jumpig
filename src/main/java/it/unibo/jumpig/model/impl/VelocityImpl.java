0ac04c430d436fc1a6250b61cc812fbec89d6415
Alessandro <alessandro.verna@studio.unibo.it>
2023-05-03 21:22:08 +0200

    Added javadoc in computeHorizontalVelocity

--- src/main/java/it/unibo/jumpig/model/impl/VelocityImpl.java
+++ src/main/java/it/unibo/jumpig/model/impl/VelocityImpl.java
@@ -99,8 +99,11 @@ public class VelocityImpl implements Velocity {
         return Double.doubleToLongBits(this.componentY) == Double.doubleToLongBits(other.componentY);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void computeHorizontalVelocity(int direction) {
+    public void computeHorizontalVelocity(final int direction) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'computeHorizontalVelocity'");
     }
