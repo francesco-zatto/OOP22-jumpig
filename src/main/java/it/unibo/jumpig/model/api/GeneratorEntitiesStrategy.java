639afd8cb8562280d29a1163089defcec5e77b6f
Francesco <francesco.filippini8@studio.unibo.it>
2023-05-07 17:21:26 +0200

    Moved method checkEqualsPosition in GeneratorEntitiesStrategy

--- src/main/java/it/unibo/jumpig/model/api/GeneratorEntitiesStrategy.java
+++ src/main/java/it/unibo/jumpig/model/api/GeneratorEntitiesStrategy.java
@@ -2,7 +2,9 @@ package it.unibo.jumpig.model.api;
 
 import java.util.Set;
 
+import it.unibo.jumpig.common.api.Position;
 import it.unibo.jumpig.common.api.hitbox.Hitbox;
+import it.unibo.jumpig.common.impl.PositionImpl;
 import it.unibo.jumpig.model.api.gameentity.GameEntity;
 
 /**
@@ -17,5 +19,23 @@ public interface GeneratorEntitiesStrategy<H extends Hitbox, G extends GameEntit
      * @return the generated set of game entity
      */
     Set<G> generate();
+
+    /**
+     * The method to generate coordinates (for a generic entity) without generating entities on the same y.
+     * In this way I will not generate colliding entities.
+     * @param startEntity the coordinates of the entity I'm going to generate.
+     * @param maxWidth the width of the game
+     * @param maxHeight the height of the game
+     * @param setentities the set of each entity's position
+     * @param camera the camera of the game
+     * @return  the coordinates I'm going to generate
+     */
+    default Position checkEqualsPosition(final Position startEntity, final double maxWidth, final double maxHeight, final Set<Position> setentities, final Camera camera) {
+        return setentities.stream()
+            .anyMatch(x -> x.getY() == startEntity.getY()) 
+                ? this.checkEqualsPosition(new PositionImpl(Math.random() * maxWidth, 
+                        Math.random() * maxHeight * 3 + camera.getCameraHeight()), maxWidth, maxHeight, setentities, camera) 
+                : startEntity;
+    }
 
 }
