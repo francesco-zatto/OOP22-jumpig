67b150275fd8c0c41c92443174271be49641b439
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2023-05-09 22:02:04 +0200

    Added testManyEnemyCollisionsInDifferentPositions method

--- src/test/java/collision/EnemyCollisionHandlerTest.java
+++ src/test/java/collision/EnemyCollisionHandlerTest.java
@@ -3,12 +3,14 @@ package collision;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 
 import it.unibo.jumpig.common.api.Position;
 import it.unibo.jumpig.common.impl.PositionImpl;
+import it.unibo.jumpig.common.impl.hitbox.RectangleHitbox;
 import it.unibo.jumpig.model.api.gameentity.Enemy;
 import it.unibo.jumpig.model.impl.gameentity.EnemyImpl;
 import it.unibo.jumpig.model.impl.gameentity.PlayerImpl;
@@ -35,13 +37,39 @@ class EnemyCollisionHandlerTest {
     @Test
     void testManyEnemyCollisions() {
         final var player = new PlayerImpl(PLAYER_POSITION);
-        final List<? extends Enemy> enemies = Stream.iterate(0, i -> i + 1)
-                .limit(player.getLives())
-                .map(i -> ENEMY_POSITION)
-                .map(EnemyImpl::new)
-                .toList();
+        final List<? extends Enemy> enemies = createEnemies(player.getLives(), () -> ENEMY_POSITION);
         enemies.forEach(e -> e.handleCollision(player));
         enemies.forEach(CoinCollisionHandlerTest::assertIsTaken);
         assertEquals(0, player.getLives());
+    }
+
+    @Test
+    void testManyEnemyCollisionsInDifferentPositions() {
+        final var player = new PlayerImpl(PLAYER_POSITION);
+        final int playerLives = player.getLives();
+        final int numberOfEnemies = (int) (player.getHitbox().getHeight());
+        final List<? extends Enemy> enemies = createEnemies(numberOfEnemies, insidePlayerPositions(player.getHitbox()));
+        enemies.forEach(e -> e.handleCollision(player));
+        enemies.forEach(CoinCollisionHandlerTest::assertIsTaken);
+        assertEquals(playerLives - numberOfEnemies, player.getLives());
+    }
+
+    private Supplier<Position> insidePlayerPositions(final RectangleHitbox player) {
+        return new Supplier<>() {
+
+            private double enemyOrdinate = player.getRectangleLowerY();
+
+            @Override
+            public Position get() {
+                return new PositionImpl(player.getCenter().getX(), this.enemyOrdinate++);
+            }
+        };
+    }
+
+    private List<? extends Enemy> createEnemies(final int numberOfEnemies, final Supplier<Position> positionSupplier) {
+        return Stream.generate(positionSupplier)
+                .limit(numberOfEnemies)
+                .map(EnemyImpl::new)
+                .toList();
     }
 }
