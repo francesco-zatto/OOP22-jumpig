ad4fccc72d5a1664d5fcc14f4dd42175edeac14c
Alessandro <alessandro.verna@studio.unibo.it>
2023-05-10 12:01:40 +0200

    Created enum Direction.java

---
+++ src/main/java/it/unibo/jumpig/common/impl/Direction.java
@@ -0,0 +1,17 @@
+package it.unibo.jumpig.common.impl;
+
+public enum Direction {
+    HORIZONTAL_SX(-1),
+    HORIZONTAL_DX(1),
+    HORIZONTAL_NULL(0);
+
+    private final int actualDirection;
+
+    private Direction(int actualDirection) {
+        this.actualDirection = actualDirection;
+    }
+
+    public int getDirection() {
+        return this.actualDirection;
+    }
+}
