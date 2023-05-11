5e60b28c1c74481a602e186d2b088ad4b8270b12
Alessandro <alessandro.verna@studio.unibo.it>
2023-05-11 13:40:12 +0200

    Added javadoc

--- src/main/java/it/unibo/jumpig/controller/impl/LeaderBoardControllerImpl.java
+++ src/main/java/it/unibo/jumpig/controller/impl/LeaderBoardControllerImpl.java
@@ -2,18 +2,26 @@ package it.unibo.jumpig.controller.impl;
 
 import it.unibo.jumpig.controller.api.LeaderboardController;
 
+/**
+ * Class that controls leaderboard interactions.
+ */
 public class LeaderBoardControllerImpl implements LeaderboardController {
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void start() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'start'");
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void close() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'close'");
     }
-    
 }
