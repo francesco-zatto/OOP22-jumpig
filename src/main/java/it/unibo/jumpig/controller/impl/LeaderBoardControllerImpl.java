1547cc82767280f05e4e9b2a7851232b809fbee1
Alessandro <alessandro.verna@studio.unibo.it>
2023-05-11 13:36:17 +0200

    Created class LeaderboardControllerImpl

---
+++ src/main/java/it/unibo/jumpig/controller/impl/LeaderBoardControllerImpl.java
@@ -0,0 +1,19 @@
+package it.unibo.jumpig.controller.impl;
+
+import it.unibo.jumpig.controller.api.LeaderboardController;
+
+public class LeaderBoardControllerImpl implements LeaderboardController {
+
+    @Override
+    public void start() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+
+    @Override
+    public void close() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'close'");
+    }
+    
+}
