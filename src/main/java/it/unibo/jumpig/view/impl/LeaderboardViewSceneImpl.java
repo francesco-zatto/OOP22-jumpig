cdde9ba4558334064ca12bb87b2a854729d428c3
Alessandro <alessandro.verna@studio.unibo.it>
2023-05-16 17:34:53 +0200

    Added button exit and parameter controller in constructor

--- src/main/java/it/unibo/jumpig/view/impl/LeaderboardViewSceneImpl.java
+++ src/main/java/it/unibo/jumpig/view/impl/LeaderboardViewSceneImpl.java
@@ -1,16 +1,19 @@
 package it.unibo.jumpig.view.impl;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.Toolkit;
 
 import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
+import it.unibo.jumpig.controller.api.LeaderboardController;
 import it.unibo.jumpig.model.api.Leaderboard;
 import it.unibo.jumpig.model.api.Score;
 import it.unibo.jumpig.view.api.LeaderboardViewScene;
@@ -23,17 +26,26 @@ public class LeaderboardViewSceneImpl implements LeaderboardViewScene {
     private static final String FRAME_TITLE = "Leaderboard";
     private static final double ASPECT_RATIO = 16.0 / 9.0;
     private static final double SCREEN_FRACTION = 5;
+    private final LeaderboardController controller;
     private final JFrame frame = new JFrame(FRAME_TITLE);
     private final JPanel mainPanel = new JPanel();
     /**
      * Constructor for LeaderboardViewSceneImpl.
+     * @param controller the leaderboard controller
      * @param leaderboard the leaderboard
      */
-    public LeaderboardViewSceneImpl(final Leaderboard leaderboard) {
+    public LeaderboardViewSceneImpl(
+        final LeaderboardController controller,
+        final Leaderboard leaderboard
+        ) {
+        this.controller = controller;
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        frame.setLayout(new BorderLayout());
+        mainPanel.setLayout(new GridLayout(1, 0));
         createLeaderboardOfScores(mainPanel, leaderboard);
-        frame.getContentPane().add(mainPanel);
+        frame.getContentPane().add(mainPanel, BorderLayout.CENTER);
+        final JButton exitButton = new JButton("EXIT");
+        frame.getContentPane().add(exitButton, BorderLayout.SOUTH);
         final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
         final Dimension startScreen = new Dimension(
             (int) (screen.getWidth() / SCREEN_FRACTION), 
@@ -43,6 +55,7 @@ public class LeaderboardViewSceneImpl implements LeaderboardViewScene {
         frame.setLocationByPlatform(true);
         frame.setPreferredSize(frame.getSize());
         frame.setResizable(true);
+        exitButton.addActionListener(e -> this.controller.close());
     }
 
     /**
