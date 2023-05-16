ce3b7667a420aa9411420984a67f88283d3d671a
Alessandro <alessandro.verna@studio.unibo.it>
2023-05-16 15:24:27 +0200

    Removed field leaderboard and fixed javadoc

--- src/main/java/it/unibo/jumpig/view/impl/LeaderboardViewSceneImpl.java
+++ src/main/java/it/unibo/jumpig/view/impl/LeaderboardViewSceneImpl.java
@@ -12,19 +12,24 @@ import javax.swing.WindowConstants;
 import it.unibo.jumpig.model.api.Leaderboard;
 import it.unibo.jumpig.view.api.LeaderboardViewScene;
 
+/**
+ * Class that views the leaderboard.
+ */
 public class LeaderboardViewSceneImpl implements LeaderboardViewScene {
 
     private static final String FRAME_TITLE = "Leaderboard";
     private static final double ASPECT_RATIO = 16.0 / 9.0;
     private static final double SCREEN_FRACTION = 5;
     private final JFrame frame = new JFrame(FRAME_TITLE);
-    private final Leaderboard leaderboard;
+    /**
+     * Constructor for LeaderboardViewSceneImpl.
+     * @param leaderboard the leaderboard
+     */
     public LeaderboardViewSceneImpl(final Leaderboard leaderboard) {
-        this.leaderboard = leaderboard.copy();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        JPanel mainPanel = new JPanel();
+        final JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new GridBagLayout());
-        JLabel leaderboardLabel = new JLabel(this.leaderboard.toString());
+        final JLabel leaderboardLabel = new JLabel(leaderboard.toString());
         mainPanel.add(leaderboardLabel);
         frame.getContentPane().add(mainPanel);
         final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
@@ -37,14 +42,20 @@ public class LeaderboardViewSceneImpl implements LeaderboardViewScene {
         frame.setPreferredSize(frame.getSize());
         frame.setResizable(true);
     }
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void show() {
         frame.setVisible(true);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void quit() {
         frame.dispose();
     }
-    
 }
