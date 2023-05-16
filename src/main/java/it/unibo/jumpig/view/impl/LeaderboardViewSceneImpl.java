b8fb6821e1be295a905167e743c5d0347f994b42
Alessandro <alessandro.verna@studio.unibo.it>
2023-05-16 14:37:19 +0200

    Created field frame

--- src/main/java/it/unibo/jumpig/view/impl/LeaderboardViewSceneImpl.java
+++ src/main/java/it/unibo/jumpig/view/impl/LeaderboardViewSceneImpl.java
@@ -1,10 +1,13 @@
 package it.unibo.jumpig.view.impl;
 
+import javax.swing.JFrame;
+
 import it.unibo.jumpig.model.api.Leaderboard;
 import it.unibo.jumpig.view.api.LeaderboardViewScene;
 
 public class LeaderboardViewSceneImpl implements LeaderboardViewScene {
 
+    private final JFrame frame = new JFrame();
     public LeaderboardViewSceneImpl(final Leaderboard leaderboard) {
 
     }
