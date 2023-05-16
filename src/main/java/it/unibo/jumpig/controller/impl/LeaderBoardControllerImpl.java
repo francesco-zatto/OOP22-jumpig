e83bcfbb1e403cd1433d38f912bf8874964750f9
Alessandro <alessandro.verna@studio.unibo.it>
2023-05-16 15:04:11 +0200

    Created constructor and field leaderboardView

--- src/main/java/it/unibo/jumpig/controller/impl/LeaderBoardControllerImpl.java
+++ src/main/java/it/unibo/jumpig/controller/impl/LeaderBoardControllerImpl.java
@@ -1,12 +1,19 @@
 package it.unibo.jumpig.controller.impl;
 
 import it.unibo.jumpig.controller.api.LeaderboardController;
+import it.unibo.jumpig.model.api.Leaderboard;
+import it.unibo.jumpig.view.api.LeaderboardViewScene;
+import it.unibo.jumpig.view.impl.LeaderboardViewSceneImpl;
 
 /**
  * Class that controls leaderboard interactions.
  */
 public class LeaderBoardControllerImpl implements LeaderboardController {
 
+    private final LeaderboardViewScene leaderboardView;
+    public LeaderBoardControllerImpl(final Leaderboard leaderboard) {
+        this.leaderboardView = new LeaderboardViewSceneImpl(leaderboard);
+    }
     /**
      * {@inheritDoc}
      */
