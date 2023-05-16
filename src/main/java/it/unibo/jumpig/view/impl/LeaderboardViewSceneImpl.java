8e9da10865edfa19e7dd02dfad74eabb2893ccff
Alessandro <alessandro.verna@studio.unibo.it>
2023-05-16 14:32:37 +0200

    Created class LeaderboardViewSceneImpl

---
+++ src/main/java/it/unibo/jumpig/view/impl/LeaderboardViewSceneImpl.java
@@ -0,0 +1,19 @@
+package it.unibo.jumpig.view.impl;
+
+import it.unibo.jumpig.view.api.LeaderboardViewScene;
+
+public class LeaderboardViewSceneImpl implements LeaderboardViewScene {
+
+    @Override
+    public void show() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'show'");
+    }
+
+    @Override
+    public void quit() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'quit'");
+    }
+    
+}
